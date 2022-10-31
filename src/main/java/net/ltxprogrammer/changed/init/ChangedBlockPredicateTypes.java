25f8623955f165687839fe72f3f9ca3f7b072d07
LtxPgm <ltxprogrammer@outlook.com>
2022-10-31 16:01:33 -0500

    Removed the need for Dynamic.java

--- src/main/java/net/ltxprogrammer/changed/init/ChangedBlockPredicateTypes.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedBlockPredicateTypes.java
@@ -1,23 +1,24 @@
 package net.ltxprogrammer.changed.init;
 
 import com.mojang.serialization.Codec;
-import net.ltxprogrammer.changed.util.Dynamic;
+import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicateType;
-import net.minecraft.world.level.levelgen.blockpredicates.WouldSurvivePredicate;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ChangedBlockPredicateTypes {
-    public static BlockPredicateType<ChangedFeatures.DeferredWouldSurvivePredicate> DEFERRED_WOULD_SURVIVE = register("changed:would_survive", ChangedFeatures.DeferredWouldSurvivePredicate.CODEC);
+    public static BlockPredicateType<ChangedFeatures.DeferredWouldSurvivePredicate> DEFERRED_WOULD_SURVIVE =
+            register("changed:would_survive", ChangedFeatures.DeferredWouldSurvivePredicate.CODEC);
 
-    private static <P extends BlockPredicate> BlockPredicateType<P> register(String p_190450_, Codec<P> p_190451_) {
-        return (BlockPredicateType<P>) Dynamic.lateRegister(Registry.BLOCK_PREDICATE_TYPES, new ResourceLocation(p_190450_), () -> () -> (Codec<BlockPredicate>) p_190451_);
+    private static <P extends BlockPredicate> BlockPredicateType<P> register(String name, Codec<P> codec) {
+        BlockPredicateType<P> ret = null;
+        if (Registry.BLOCK_PREDICATE_TYPES instanceof MappedRegistry<BlockPredicateType<?>> reg) {
+            reg.unfreeze();
+            ret = Registry.register(reg, name, () -> codec);
+            reg.freeze();
+        }
+        return ret;
     }
 }
