b876f5f032db9a2931c374aea46faf4285cadbd0
LtxPgm <ltxprogrammer@outlook.com>
2022-11-26 12:11:25 -0600

    Added aqua affinity for all aquatic latexes; gave felines night vision
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

---
+++ src/main/java/net/ltxprogrammer/changed/mixin/EnchantmentHelperMixin.java
@@ -0,0 +1,19 @@
+package net.ltxprogrammer.changed.mixin;
+
+import net.ltxprogrammer.changed.process.ProcessTransfur;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EnchantmentHelper.class)
+public class EnchantmentHelperMixin {
+    @Inject(method = "hasAquaAffinity", at = @At("HEAD"), cancellable = true)
+    private static void hasAquaAffinity(LivingEntity le, CallbackInfoReturnable<Boolean> callback) {
+        if (le instanceof Player player && ProcessTransfur.isPlayerLatex(player) && ProcessTransfur.getPlayerLatexVariant(player).breatheMode.hasAquaAffinity())
+            callback.setReturnValue(true);
+    }
+}
