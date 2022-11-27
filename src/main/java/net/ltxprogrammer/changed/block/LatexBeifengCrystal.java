876c864bac5fa86b27bb26561fbe6d804d04278f
LtxPgm <ltxprogrammer@outlook.com>
2022-11-27 09:41:57 -0600

    Added small versions of wolf and beifeng crystal; adjusted block drops
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/block/LatexBeifengCrystal.java
+++ src/main/java/net/ltxprogrammer/changed/block/LatexBeifengCrystal.java
@@ -1,9 +1,10 @@
 package net.ltxprogrammer.changed.block;
 
 import net.ltxprogrammer.changed.entity.variant.LatexVariant;
+import net.ltxprogrammer.changed.init.ChangedItems;
 
 public class LatexBeifengCrystal extends AbstractDoubleLatexCrystal {
     public LatexBeifengCrystal(Properties p_53514_) {
-        super(LatexVariant.LATEX_BEIFENG, p_53514_);
+        super(LatexVariant.LATEX_BEIFENG, ChangedItems.LATEX_BEIFENG_CRYSTAL_FRAGMENT, p_53514_);
     }
 }
--- src/main/java/net/ltxprogrammer/changed/entity/beast/LatexBeifeng.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexBeifeng.java
@@ -4,7 +4,7 @@ import net.ltxprogrammer.changed.init.ChangedParticles;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
-public class LatexBeifeng extends AbstractLatexWolf {
+public class LatexBeifeng extends AbstractLatexWolf implements DarkLatexEntity {
     public LatexBeifeng(EntityType<? extends LatexBeifeng> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
     }
@@ -12,5 +12,10 @@ public class LatexBeifeng extends AbstractLatexWolf {
     @Override
     public ChangedParticles.Color3 getDripColor() {
         return level.random.nextInt(10) > 3 ? ChangedParticles.Color3.BLUE : ChangedParticles.Color3.WHITE;
+    }
+
+    @Override
+    public boolean isMaskless() {
+        return true;
     }
 }
