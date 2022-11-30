c1aea9921e9e968bc4ef1233f73bf0790980705c
BaiTian6641 <weystoru6641@163.com>
2022-11-30 23:41:08 +0800

    Infuser and Purifier update. (#55)
    * Chinese Translation.
    
    Almost done.
    
    * Update zh_cn.json
    
    * Add new texture for infuser and purifier
    
    Textures by valurio
    
    * Update infuser and purifier
    
    * Update infuser and purifier
    
    * Add dark latex mask advancement
    
    * Update infuser and purifier java code
    
    * Update zh_cn.json for new block
    
    * Update purifier.json
    
    * Add files via upload

--- src/main/java/net/ltxprogrammer/changed/block/Infuser.java
+++ src/main/java/net/ltxprogrammer/changed/block/Infuser.java
@@ -13,8 +13,21 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.Direction;
 
 public class Infuser extends LabBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public Infuser(Properties p_49795_) {
         super(p_49795_);
     }
@@ -30,6 +43,24 @@ public class Infuser extends LabBlock {
             player.openMenu(state.getMenuProvider(level, pos));
             return InteractionResult.CONSUME;
         }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
     }
 
     @Override
--- src/main/java/net/ltxprogrammer/changed/block/Purifier.java
+++ src/main/java/net/ltxprogrammer/changed/block/Purifier.java
@@ -26,6 +26,18 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.Direction;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -33,6 +45,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Purifier extends BaseEntityBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public Purifier(BlockBehaviour.Properties p_49795_) {
         super(p_49795_.requiresCorrectToolForDrops());
         this.registerDefaultState(this.stateDefinition.any());
@@ -66,10 +79,6 @@ public class Purifier extends BaseEntityBlock {
         return new ArrayList<>(Collections.singleton(this.asItem().getDefaultInstance()));
     }
 
-    public BlockState getStateForPlacement(BlockPlaceContext p_48689_) {
-        return this.defaultBlockState();
-    }
-
     public void setPlacedBy(Level p_48694_, BlockPos p_48695_, BlockState p_48696_, LivingEntity p_48697_, ItemStack p_48698_) {
         if (p_48698_.hasCustomHoverName()) {
             BlockEntity blockentity = p_48694_.getBlockEntity(p_48695_);
@@ -78,6 +87,25 @@ public class Purifier extends BaseEntityBlock {
             }
         }
 
+    }
+
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
     }
 
     public void onRemove(BlockState p_48713_, Level p_48714_, BlockPos p_48715_, BlockState p_48716_, boolean p_48717_) {
