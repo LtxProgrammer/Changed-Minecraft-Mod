a8ff6e48d650acb5ec408dff5c2176134dd09e61
LtxPgm <ltxprogrammer@outlook.com>
2022-12-13 13:59:34 -0600

    Fixed #110; fixed #111
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/block/LabTable2x1.java
+++ src/main/java/net/ltxprogrammer/changed/block/LabTable2x1.java
@@ -3,6 +3,7 @@ package net.ltxprogrammer.changed.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
@@ -12,8 +13,11 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -91,8 +95,8 @@ public class LabTable2x1 extends AbstractCustomShapeBlock {
     protected BlockPos getBlockPos(BlockState p_52776_, BlockPos p_52778_, int section_num) {
         int x = 0;
         int self_num = p_52776_.getValue(SECTION);
-        if (self_num <= 1 && section_num > 1) x = 1;
-        else if (self_num > 1 && section_num <= 1) x = -1;
+        if (self_num == 0 && section_num == 1) x = -1;
+        else if (self_num == 1 && section_num == 0) x = 1;
 
         switch (p_52776_.getValue(FACING)) {
             case NORTH -> {
@@ -133,6 +137,37 @@ public class LabTable2x1 extends AbstractCustomShapeBlock {
 
         return (this.getBlockState(p_52783_, p_52784_, p_52785_, 0).is(this) || section == 0)
                 && (this.getBlockState(p_52783_, p_52784_, p_52785_, 1).is(this) || section == 1);
+    }
+
+    protected void preventCreativeDropFromBottomPart(Level p_52904_, BlockPos p_52905_, BlockState p_52906_, Player p_52907_) {
+        int section = p_52906_.getValue(SECTION);
+        if (section == 1) {
+            BlockPos blockpos = getBlockPos(p_52906_, p_52905_, 0);
+            BlockState blockstate = p_52904_.getBlockState(blockpos);
+            if (blockstate.is(p_52906_.getBlock()) && blockstate.getValue(SECTION) == 0) {
+                BlockState blockstate1 = blockstate.hasProperty(BlockStateProperties.WATERLOGGED) && blockstate.getValue(BlockStateProperties.WATERLOGGED) ? Blocks.WATER.defaultBlockState() : Blocks.AIR.defaultBlockState();
+                p_52904_.setBlock(blockpos, blockstate1, 35);
+                p_52904_.levelEvent(p_52907_, 2001, blockpos, Block.getId(blockstate));
+            }
+        }
+
+    }
+
+    public void playerWillDestroy(Level p_52878_, BlockPos p_52879_, BlockState p_52880_, Player p_52881_) {
+        if (!p_52878_.isClientSide) {
+            if (p_52881_.isCreative()) {
+                preventCreativeDropFromBottomPart(p_52878_, p_52879_, p_52880_, p_52881_);
+            } else if (p_52880_.getValue(SECTION) == 1) {
+                dropResources(p_52880_, p_52878_, p_52879_, (BlockEntity)null, p_52881_, p_52881_.getMainHandItem());
+            }
+        }
+
+        super.playerWillDestroy(p_52878_, p_52879_, p_52880_, p_52881_);
+    }
+
+    public void destroy(LevelAccessor p_49860_, BlockPos p_49861_, BlockState p_49862_) {
+        if (p_49862_.getValue(SECTION) == 1)
+            p_49860_.destroyBlock(getBlockPos(p_49862_, p_49861_, 0), true);
     }
 
     public RenderShape getRenderShape(BlockState p_54559_) {
@@ -177,9 +212,9 @@ public class LabTable2x1 extends AbstractCustomShapeBlock {
     public BlockState updateShape(BlockState p_52796_, Direction p_52797_, BlockState p_52798_, LevelAccessor p_52799_, BlockPos p_52800_, BlockPos p_52801_) {
         int section = p_52796_.getValue(SECTION);
         Direction face = p_52796_.getValue(FACING);
-        if (p_52797_.getAxis() == Direction.Axis.Y && section == 0 == (p_52797_ == Direction.UP)) {
-            return p_52798_.is(this) && p_52798_.getValue(SECTION) != section ? p_52796_.setValue(FACING, p_52798_.getValue(FACING)) : Blocks.AIR.defaultBlockState();
-        } else {
+        if (p_52797_.getAxis() == Direction.Axis.Y)
+            return super.updateShape(p_52796_, p_52797_, p_52798_, p_52799_, p_52800_, p_52801_);
+        else {
             if ((face == Direction.NORTH) && p_52797_.getAxis() == Direction.Axis.X) {
                 if (section < 1 && p_52797_ == Direction.EAST) return super.updateShape(p_52796_, p_52797_, p_52798_, p_52799_, p_52800_, p_52801_);
                 else if ( p_52797_ == Direction.WEST) return super.updateShape(p_52796_, p_52797_, p_52798_, p_52799_, p_52800_, p_52801_);
