30d6efac4264fa8f0023e051ec0fc65d12bc12ce
LtxPgm <ltxprogrammer@outlook.com>
2022-12-13 23:22:50 -0600

    Implemented and fixed #118
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedDamageSources.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedDamageSources.java
@@ -3,5 +3,6 @@ package net.ltxprogrammer.changed.init;
 import net.minecraft.world.damagesource.DamageSource;
 
 public class ChangedDamageSources {
-    public static final DamageSource TRANSFUR = (new DamageSource("transfur")).bypassArmor().bypassInvul().bypassMagic();
+    public static final DamageSource TRANSFUR = (new DamageSource("transfur")).bypassArmor().bypassMagic();
+    public static final DamageSource WHITE_LATEX = (new DamageSource("white_latex")).bypassArmor().bypassMagic();
 }
