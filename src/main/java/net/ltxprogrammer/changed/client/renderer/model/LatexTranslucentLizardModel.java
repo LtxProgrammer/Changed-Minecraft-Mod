3613e2fd11127077b73235d51079abfe0e4445f9
bijimio <117211742+bijimio@users.noreply.github.com>
2022-12-29 12:46:39 -0800

    Bijimio Experimental Fork V1.1.5 (#151)
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/model/LatexTranslucentLizardModel.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/LatexTranslucentLizardModel.java
@@ -96,7 +96,7 @@ public class LatexTranslucentLizardModel extends LatexHumanoidModel<LatexTranslu
 
         PartDefinition Gooeycenter_r2 = LeftLeg.addOrReplaceChild("Gooeycenter_r2", CubeListBuilder.create().texOffs(44, 46).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.2182F, 0.0F, 0.0F));
 
-        PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -7.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
+        PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -7.005F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
                 .texOffs(32, 12).addBox(-2.0F, -2.0F, -6.0F, 4.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 5).addBox(-1.5F, 0.0F, -5.0F, 3.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.0F, 0.0F));
 
@@ -121,16 +121,16 @@ public class LatexTranslucentLizardModel extends LatexHumanoidModel<LatexTranslu
         PartDefinition Base_r4 = Tail.addOrReplaceChild("Base_r4", CubeListBuilder.create().texOffs(48, 19).addBox(-2.0F, -2.8F, 0.4F, 4.0F, 4.0F, 4.0F, new CubeDeformation(-0.3F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.5236F, 0.0F, 0.0F));
 
         PartDefinition RightArm = partdefinition.addOrReplaceChild("RightArm", CubeListBuilder.create().texOffs(20, 28).addBox(-3.0F, -1.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 34).addBox(0.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(32, 30).addBox(-3.0F, 10.85F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(32, 28).addBox(-3.0F, 10.85F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 32).addBox(-3.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, 0.0F, 0.0F));
+                .texOffs(0, 34).addBox(0.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(32, 30).addBox(-3.0F, 10.85F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(32, 28).addBox(-3.0F, 10.85F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(16, 32).addBox(-3.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F)), PartPose.offset(-5.0F, 0.0F, 0.0F));
 
         PartDefinition LeftArm = partdefinition.addOrReplaceChild("LeftArm", CubeListBuilder.create().texOffs(0, 32).addBox(-1.0F, -1.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 36).addBox(-1.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(35, 29).addBox(2.0F, 10.85F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 34).addBox(2.0F, 10.85F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 34).addBox(2.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, 0.0F, 0.0F));
+                .texOffs(16, 36).addBox(-1.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(35, 29).addBox(2.0F, 10.85F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(16, 34).addBox(2.0F, 10.85F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F))
+                .texOffs(12, 34).addBox(2.0F, 10.85F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.01F)), PartPose.offset(5.0F, 0.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 128, 128);
     }
