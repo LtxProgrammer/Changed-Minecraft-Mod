c7dc032f0a15572460c938fd0dc26a85ea56f6d8
LtxPgm <ltxprogrammer@outlook.com>
2023-01-02 14:17:02 -0600

    Implemented #147
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/item/WearableItem.java
+++ src/main/java/net/ltxprogrammer/changed/item/WearableItem.java
@@ -2,6 +2,7 @@ package net.ltxprogrammer.changed.item;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,14 +10,22 @@ import net.minecraftforge.fml.common.Mod;
 public interface WearableItem {
     void wearTick(LivingEntity entity, ItemStack itemStack);
     boolean customWearRenderer();
+    boolean allowedToKeepWearing(LivingEntity entity);
 
     @Mod.EventBusSubscriber
     class Event {
         @SubscribeEvent
         public static void onEntityTick(LivingEvent.LivingUpdateEvent event) {
             event.getEntityLiving().getArmorSlots().forEach(itemStack -> {
-                if (itemStack.getItem() instanceof WearableItem wearableBlock) {
-                    wearableBlock.wearTick(event.getEntityLiving(), itemStack);
+                if (itemStack.getItem() instanceof WearableItem wearableItem) {
+                    if (!wearableItem.allowedToKeepWearing(event.getEntityLiving())) {
+                        ItemStack nStack = itemStack.copy();
+                        Block.popResource(event.getEntityLiving().getLevel(), event.getEntityLiving().blockPosition(), nStack);
+                        itemStack.setCount(0);
+                        return;
+                    }
+
+                    wearableItem.wearTick(event.getEntityLiving(), itemStack);
                 }
             });
         }
