7031f1c737e2696971eb62b169f56b9243caa071
Bijimio <mp39@tutanota.com>
2023-01-03 18:33:27 -0800

    Bijimio Experimental Fork V1.2.0

---
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSniperDog.java
@@ -0,0 +1,18 @@
+package net.ltxprogrammer.changed.entity.beast;
+
+import net.ltxprogrammer.changed.entity.OrganicLatex;
+import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class LatexSniperDog extends AbstractLatexWolf implements OrganicLatex {
+    public LatexSniperDog(EntityType<? extends LatexSniperDog> p_19870_, Level p_19871_) {
+        super(p_19870_, p_19871_);
+    }
+
+
+    public ChangedParticles.Color3 getDripColor() {
+        return ChangedParticles.Color3.getColor("#ef8f44");
+    }
+
+}
--- src/main/java/net/ltxprogrammer/changed/init/ChangedEntityRenderers.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedEntityRenderers.java
@@ -39,6 +39,7 @@ public class ChangedEntityRenderers {
         event.registerEntityRenderer(ChangedEntities.LATEX_SHARK_MALE.get(), LatexSharkMaleRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LATEX_SILVER_FOX.get(), LatexSilverFoxRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LATEX_SIREN.get(), LatexSirenRenderer::new);
+        event.registerEntityRenderer(ChangedEntities.LATEX_SNIPER_DOG.get(), LatexSniperDogRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LATEX_SNOW_LEOPARD_FEMALE.get(), LatexSnowLeopardFemaleRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LATEX_SNOW_LEOPARD_MALE.get(), LatexSnowLeopardMaleRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LATEX_SQUID_DOG.get(), LatexSquidDogRenderer::new);
@@ -53,6 +54,7 @@ public class ChangedEntityRenderers {
         event.registerEntityRenderer(ChangedEntities.LIGHT_LATEX_KNIGHT_FUSION.get(), LightLatexKnightFusionRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LIGHT_LATEX_WOLF_FEMALE.get(), LightLatexWolfFemaleRenderer::new);
         event.registerEntityRenderer(ChangedEntities.LIGHT_LATEX_WOLF_MALE.get(), LightLatexWolfMaleRenderer::new);
+        event.registerEntityRenderer(ChangedEntities.LIGHT_LATEX_WOLF_ORGANIC.get(), LightLatexWolfOrganicRenderer::new);
         event.registerEntityRenderer(ChangedEntities.MILK_PUDDING.get(), MilkPuddingRenderer::new);
         event.registerEntityRenderer(ChangedEntities.WHITE_LATEX_WOLF.get(), WhiteLatexWolfRenderer::new);
 
