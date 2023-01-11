b6bbca7f9428f503a9551bdde5a461d7f9e49eeb
Bijimio <mp39@tutanota.com>
2023-01-11 05:14:16 -0800

    Bijimio Experimental Fork 1.3.5

--- src/main/java/net/ltxprogrammer/changed/entity/beast/LatexYuin.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexYuin.java
@@ -2,6 +2,7 @@ package net.ltxprogrammer.changed.entity.beast;
 
 
 import net.ltxprogrammer.changed.entity.LatexEntity;
+import net.ltxprogrammer.changed.entity.PowderSnowWalkable;
 import net.ltxprogrammer.changed.entity.LatexType;
 import net.ltxprogrammer.changed.entity.TransfurMode;
 import net.ltxprogrammer.changed.init.ChangedParticles;
@@ -9,7 +10,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
 
-public class LatexYuin extends LatexEntity {
+public class LatexYuin extends LatexEntity implements PowderSnowWalkable{
     public LatexYuin(EntityType<? extends LatexYuin> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
     }
