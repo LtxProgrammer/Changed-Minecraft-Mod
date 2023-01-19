ecd22763249a139020d0a3271fc4beadd32ba338
LtxPgm <ltxprogrammer@outlook.com>
2023-01-19 12:11:24 -0600

    Hopefully fixed #204 again
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/network/packet/SyncTransfurProgressPacket.java
+++ src/main/java/net/ltxprogrammer/changed/network/packet/SyncTransfurProgressPacket.java
@@ -1,21 +1,26 @@
 package net.ltxprogrammer.changed.network.packet;
 
+import net.ltxprogrammer.changed.data.BiSignaler;
 import net.ltxprogrammer.changed.data.Signaler;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.network.NetworkEvent;
 
+import java.util.UUID;
 import java.util.function.Supplier;
 
 public class SyncTransfurProgressPacket implements ChangedPacket {
-    public static final Signaler<ProcessTransfur.TransfurProgress> SIGNAL = new Signaler<>();
+    public static final BiSignaler<UUID, ProcessTransfur.TransfurProgress> SIGNAL = new BiSignaler<>();
+    private final UUID uuid;
     private final ProcessTransfur.TransfurProgress progress;
 
-    public SyncTransfurProgressPacket(ProcessTransfur.TransfurProgress progress) {
+    public SyncTransfurProgressPacket(UUID uuid, ProcessTransfur.TransfurProgress progress) {
+        this.uuid = uuid;
         this.progress = progress;
     }
 
     public SyncTransfurProgressPacket(FriendlyByteBuf buffer) {
+        this.uuid = buffer.readUUID();
         this.progress = new ProcessTransfur.TransfurProgress(buffer.readInt(), buffer.readResourceLocation());
     }
 
@@ -28,7 +33,7 @@ public class SyncTransfurProgressPacket implements ChangedPacket {
         NetworkEvent.Context context = contextSupplier.get();
 
         if (context.getDirection().getReceptionSide().isClient()) {
-            SIGNAL.invoke(progress);
+            SIGNAL.invoke(uuid, progress);
             context.setPacketHandled(true);
         }
     }
