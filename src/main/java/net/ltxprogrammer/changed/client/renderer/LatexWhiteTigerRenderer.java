1fa6ac1b42fde047829e1f93c2cdcc47b29f9de0
bijimio <117211742+bijimio@users.noreply.github.com>
2023-01-22 20:51:04 -0800

    Added table recipe, fixed lang (#217)
    * Bijimio Experimental Fork V1.8.6
    
    * Bijimio Experimental Fork V1.8.7
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

---
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexWhiteTigerRenderer.java
@@ -0,0 +1,22 @@
+package net.ltxprogrammer.changed.client.renderer;
+
+import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.model.LatexRedPandaModel;
+import net.ltxprogrammer.changed.client.renderer.model.LatexWhiteTigerModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexSnowLeopardModel;
+import net.ltxprogrammer.changed.entity.beast.LatexRedPanda;
+import net.ltxprogrammer.changed.entity.beast.LatexWhiteTiger;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+public class LatexWhiteTigerRenderer extends LatexHumanoidRenderer<LatexWhiteTiger, LatexWhiteTigerModel, ArmorLatexSnowLeopardModel<LatexWhiteTiger>> {
+    public LatexWhiteTigerRenderer(EntityRendererProvider.Context context) {
+        super(context, new LatexWhiteTigerModel(context.bakeLayer(LatexWhiteTigerModel.LAYER_LOCATION)),
+                ArmorLatexSnowLeopardModel::new, ArmorLatexSnowLeopardModel.INNER_ARMOR, ArmorLatexSnowLeopardModel.OUTER_ARMOR, 0.5f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(LatexWhiteTiger p_114482_) {
+        return Changed.modResource("textures/latex_white_tiger.png");
+    }
+}
