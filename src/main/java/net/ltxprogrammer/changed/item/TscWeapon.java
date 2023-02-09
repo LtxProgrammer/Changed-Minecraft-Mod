928da3cf28d259b47e71b68d778000f7d5885acc
LtxPgm <ltxprogrammer@outlook.com>
2023-02-09 11:39:41 -0600

    Added shock sound for tsc weapons.
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/item/TscWeapon.java
+++ src/main/java/net/ltxprogrammer/changed/item/TscWeapon.java
@@ -2,12 +2,15 @@ package net.ltxprogrammer.changed.item;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.ltxprogrammer.changed.init.ChangedEffects;
 import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.ltxprogrammer.changed.init.ChangedSounds;
 import net.ltxprogrammer.changed.init.ChangedTabs;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -47,5 +50,10 @@ public abstract class TscWeapon extends Item implements Vanishable {
         if (source.level instanceof ServerLevel serverLevel) {
             serverLevel.sendParticles(ChangedParticles.TSC_SWEEP_ATTACK, source.getX() + d0, source.getY(0.5D), source.getZ() + d1, 0, d0, 0.0D, d1, 0.0D);
         }
+    }
+
+    public void applyShock(LivingEntity enemy) {
+        ChangedSounds.broadcastSound(enemy, ChangedSounds.PARALYZE1, 1, 1);
+        enemy.addEffect(new MobEffectInstance(ChangedEffects.SHOCK, 10, 0, false, false, true));
     }
 }
