67a4c947e833877d321fae2cf42ab66f680aec7f
LtxPgm <ltxprogrammer@outlook.com>
2023-02-09 14:30:51 -0600

    Added slither ability for snake (needs texture)
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSnake.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSnake.java
@@ -1,14 +1,20 @@
 package net.ltxprogrammer.changed.entity.beast;
 
 import net.ltxprogrammer.changed.entity.LatexEntity;
+import net.ltxprogrammer.changed.entity.LatexType;
 import net.ltxprogrammer.changed.entity.TransfurMode;
 import net.ltxprogrammer.changed.init.ChangedParticles;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
-public class LatexSnake extends LatexMantaRayFemale {
-    public LatexSnake(EntityType<? extends LatexSnake> p_19870_, Level p_19871_) {
-        super(p_19870_, p_19871_);
+public class LatexSnake extends LatexEntity {
+    public LatexSnake(EntityType<? extends LatexSnake> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public LatexType getLatexType() {
+        return LatexType.NEUTRAL;
     }
 
     @Override
@@ -19,5 +25,10 @@ public class LatexSnake extends LatexMantaRayFemale {
     @Override
     public ChangedParticles.Color3 getDripColor() {
         return ChangedParticles.Color3.getColor("#ffffff");
+    }
+
+    @Override
+    public boolean isMovingSlowly() {
+        return this.isCrouching();
     }
 }
--- src/main/java/net/ltxprogrammer/changed/init/ChangedAbilities.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedAbilities.java
@@ -18,6 +18,7 @@ public class ChangedAbilities {
     public static AccessChestAbility ACCESS_CHEST = register(new AccessChestAbility());
     public static SwitchGenderAbility SWITCH_GENDER = register(new SwitchGenderAbility());
     public static UseVariantEffectAbility USE_VARIANT_EFFECT = register(new UseVariantEffectAbility());
+    public static SlitherAbility SLITHER = register(new SlitherAbility());
 
     public static <T extends AbstractAbility<?>> T register(T ability) {
         if (REGISTRY.containsKey(ability.getId()))
