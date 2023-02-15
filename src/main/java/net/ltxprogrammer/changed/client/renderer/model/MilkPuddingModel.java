2adb61e77b7d8b1e64b0cc4c91d95ca254bbc500
LtxPgm <ltxprogrammer@outlook.com>
2023-02-15 10:51:33 -0600

    Remodeled milk pudding, and added an animation
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/model/MilkPuddingModel.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/MilkPuddingModel.java
@@ -5,43 +5,49 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.entity.beast.MilkPudding;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.util.Mth;
 
 import static net.ltxprogrammer.changed.client.renderer.model.LatexHumanoidModel.NO_DEFORMATION;
 
-public class MilkPuddingModel extends EntityModel<MilkPudding> {
+public class MilkPuddingModel extends HierarchicalModel<MilkPudding> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(Changed.modResource("milk_pudding"), "main");
-    private final ModelPart bb_main;
+    private final ModelPart Root;
+    private final ModelPart Middle;
+    private final ModelPart Top;
 
     public MilkPuddingModel(ModelPart root) {
-        this.bb_main = root.getChild("bb_main");
+        this.Root = root.getChild("Root");
+        this.Middle = Root.getChild("Middle");
+        this.Top = Middle.getChild("Top");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -5.0F, -5.0F, 10.0F, 5.0F, 10.0F, NO_DEFORMATION)
-                .texOffs(4, 2).addBox(-4.0F, -6.0F, -4.0F, 8.0F, 1.0F, 8.0F, NO_DEFORMATION)
-                .texOffs(12, 6).addBox(-2.0F, -7.0F, -2.0F, 4.0F, 1.0F, 4.0F, NO_DEFORMATION), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition Root = partdefinition.addOrReplaceChild("Root", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -4.0F, -5.0F, 10.0F, 4.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        return LayerDefinition.create(meshdefinition, 64, 64);
+        PartDefinition Middle = Root.addOrReplaceChild("Middle", CubeListBuilder.create().texOffs(0, 14).addBox(-4.0F, -5.5F, -4.0F, 8.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition Top = Middle.addOrReplaceChild("Top", CubeListBuilder.create().texOffs(24, 14).addBox(-2.0F, -7.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
     @Override
     public void setupAnim(MilkPudding entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
+        Middle.y = Mth.cos(limbSwing * 0.6662F) * limbSwingAmount;
+        Top.y = Middle.y;
     }
 
     @Override
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        bb_main.render(poseStack, buffer, packedLight, packedOverlay);
+    public ModelPart root() {
+        return Root;
     }
 }
