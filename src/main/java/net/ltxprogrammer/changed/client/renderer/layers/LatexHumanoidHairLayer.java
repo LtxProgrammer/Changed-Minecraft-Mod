203514e964ef954d9505af25163522365e3a8d0a
LtxPgm <ltxprogrammer@outlook.com>
2023-02-16 09:08:35 -0600

    (DEV) Fixed #279
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/layers/LatexHumanoidHairLayer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/layers/LatexHumanoidHairLayer.java
@@ -7,11 +7,13 @@ import net.ltxprogrammer.changed.client.renderer.model.LatexHumanoidModel;
 import net.ltxprogrammer.changed.entity.HairStyle;
 import net.ltxprogrammer.changed.entity.LatexEntity;
 import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -34,7 +36,7 @@ public class LatexHumanoidHairLayer<T extends LatexEntity, M extends LatexHumano
         });
     }
 
-    public void render(PoseStack pose, MultiBufferSource bufferSource, int packedLight, T entity, float p_116670_, float p_116671_, float p_116672_, float p_116673_, float p_116674_, float p_116675_) {
+    public void render(PoseStack pose, MultiBufferSource bufferSource, int packedLight, T entity, float p_116670_, float p_116671_, float red, float green, float blue, float alpha) {
         HairStyle style = entity.getHairStyle();
         if (style.model == null || style.textures.length == 0)
             return;
@@ -44,10 +46,11 @@ public class LatexHumanoidHairLayer<T extends LatexEntity, M extends LatexHumano
         head.translateAndRotate(pose);
         Model hair = MODEL_BY_HAIRSTYLE.get(style);
         int colorLayer = 0;
+        int overlay = LivingEntityRenderer.getOverlayCoords(entity, 0.0F);
         for (ResourceLocation layer : style.textures) {
             ChangedParticles.Color3 color = entity.getHairColor(colorLayer);
             hair.renderToBuffer(pose, bufferSource.getBuffer(RenderType.entityCutoutNoCull(layer)), packedLight,
-                    OverlayTexture.NO_OVERLAY, color.red(), color.green(), color.blue(), p_116675_);
+                    overlay, color.red(), color.green(), color.blue(), alpha);
             ++colorLayer;
         }
         pose.popPose();
