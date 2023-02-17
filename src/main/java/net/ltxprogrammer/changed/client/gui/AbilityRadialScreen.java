e34bdff13afcdebb2e90b1d996d0d29728ed9522
LtxPgm <ltxprogrammer@outlook.com>
2023-02-16 21:44:32 -0600

    Made head render for select hairstyle ability (may change to an actual texture later)
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/gui/AbilityRadialScreen.java
+++ src/main/java/net/ltxprogrammer/changed/client/gui/AbilityRadialScreen.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.player.Inventory;
 import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AbilityRadialScreen extends LatexAbilityRadialScreen<AbilityRadialMenu> {
     private final static HashMap<String, Object> guistate = ComputerMenu.guistate;
@@ -30,7 +31,9 @@ public class AbilityRadialScreen extends LatexAbilityRadialScreen<AbilityRadialM
         this.imageHeight = 0;
         this.menu = menu;
         this.variant = menu.variant;
-        this.abilities = menu.variant.abilities;
+        this.abilities = menu.variant.abilities.stream().filter(location ->
+                location != ChangedAbilities.SELECT_HAIRSTYLE.getId() || ChangedAbilities.getAbility(location).canUse(menu.player, variant))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -51,16 +54,28 @@ public class AbilityRadialScreen extends LatexAbilityRadialScreen<AbilityRadialM
             enabled = menu.variant.abilityInstances.get(abilities.get(section)).canUse();
         }
 
-        RenderSystem.setShaderTexture(0, ChangedAbilities.getAbility(abilities.get(section)).getTexture(menu.player, menu.variant));
-        if (enabled) {
-            RenderSystem.setShaderColor(0, 0, 0, 0.5f); // Render ability shadow
-            this.blit(pose, (int)x - 24 + this.leftPos, (int)y - 24 + this.topPos + 4, 0, 0, 48, 48, 48, 48);
+        if (!abilities.get(section).equals(ChangedAbilities.SELECT_HAIRSTYLE.getId())) {
+            RenderSystem.setShaderTexture(0, ChangedAbilities.getAbility(abilities.get(section)).getTexture(menu.player, menu.variant));
+            if (enabled) {
+                RenderSystem.setShaderColor(0, 0, 0, 0.5f); // Render ability shadow
+                this.blit(pose, (int)x - 24 + this.leftPos, (int)y - 24 + this.topPos + 4, 0, 0, 48, 48, 48, 48);
+            }
+            float minRed = Math.max(red, 0.125f);
+            float minGreen = Math.max(green, 0.125f);
+            float minBlue = Math.max(blue, 0.125f);
+            RenderSystem.setShaderColor(minRed, minGreen, minBlue, (enabled ? 1 : 0.5f) * alpha);
+            this.blit(pose, (int)x - 24 + this.leftPos, (int)y - 24 + this.topPos, 0, 0, 48, 48, 48, 48);
         }
-        float minRed = Math.max(red, 0.125f);
-        float minGreen = Math.max(green, 0.125f);
-        float minBlue = Math.max(blue, 0.125f);
-        RenderSystem.setShaderColor(minRed, minGreen, minBlue, (enabled ? 1 : 0.5f) * alpha);
-        this.blit(pose, (int)x - 24 + this.leftPos, (int)y - 24 + this.topPos, 0, 0, 48, 48, 48, 48);
+
+        else {
+            x = x * 0.9;
+            y = (y * 0.9) - 16;
+
+            HairStyleRadialScreen.renderEntityHeadWithHair((int)x + this.leftPos, (int)y + 32 + this.topPos, 40,
+                    (float)(this.leftPos) - mouseX + (int)x,
+                    (float)(this.topPos) - mouseY + (int)y,
+                    variant.getLatexEntity(), alpha);
+        }
     }
 
     @Override
