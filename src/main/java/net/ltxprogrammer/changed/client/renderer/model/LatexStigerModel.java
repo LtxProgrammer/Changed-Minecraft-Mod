b64d7cd79bdf722085660fdc9539d3d968d3fbf7
Bijimio <mp39@tutanota.com>
2023-02-17 06:12:55 -0800

    Bijimio Experimental Fork V2.7.0

--- src/main/java/net/ltxprogrammer/changed/client/renderer/model/LatexStigerModel.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/LatexStigerModel.java
@@ -79,6 +79,14 @@ public class LatexStigerModel extends LatexHumanoidModel<LatexStiger> implements
                 .texOffs(24, 0).addBox(-2.0F, -3.5F, -6.0F, 4.0F, 2.0F, 2.0F, CubeDeformation.NONE)
                 .texOffs(0, 5).addBox(-1.5F, -1.5F, -5.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE), PartPose.offset(0.0F, -0.5F, 0.0F));
 
+        PartDefinition leftupperantenna_r1 = Head.addOrReplaceChild("leftupperantenna_r1", CubeListBuilder.create().texOffs(0, 32).addBox(2.0F, -29.25F, -25.1F, 1.0F, 3.0F, 1.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 25.5F, 0.0F, -0.6981F, 0.0873F, 0.0F));
+
+        PartDefinition leftlowerantenna_r1 = Head.addOrReplaceChild("leftlowerantenna_r1", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -34.0F, -13.6F, 2.0F, 3.0F, 2.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 25.5F, 0.0F, -0.3054F, 0.0873F, 0.0F));
+
+        PartDefinition rightupperantenna_r1 = Head.addOrReplaceChild("rightupperantenna_r1", CubeListBuilder.create().texOffs(0, 16).addBox(-3.0F, -29.25F, -25.1F, 1.0F, 3.0F, 1.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 25.5F, 0.0F, -0.6981F, -0.0873F, 0.0F));
+
+        PartDefinition rightlowerantenna_r1 = Head.addOrReplaceChild("rightlowerantenna_r1", CubeListBuilder.create().texOffs(16, 32).addBox(-3.5F, -34.0F, -13.6F, 2.0F, 3.0F, 2.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 25.5F, 0.0F, -0.3054F, -0.0873F, 0.0F));
+
         PartDefinition Snout_r1 = Head.addOrReplaceChild("Snout_r1", CubeListBuilder.create().texOffs(47, 1).addBox(-1.0F, -29.625F, -0.95F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.5F, 25.5F, 0.0F, 0.1745F, 0.0F, 0.0F));
 
         PartDefinition RightEar = Head.addOrReplaceChild("RightEar", CubeListBuilder.create(), PartPose.offsetAndRotation(-3.0F, -8.5F, 0.0F, 0.0F, 0.0F, -0.2618F));
@@ -88,28 +96,6 @@ public class LatexStigerModel extends LatexHumanoidModel<LatexStiger> implements
         PartDefinition LeftEar = Head.addOrReplaceChild("LeftEar", CubeListBuilder.create(), PartPose.offsetAndRotation(2.5F, -8.5F, 0.0F, 0.0F, 0.0F, 0.2618F));
 
         PartDefinition leftear_r1 = LeftEar.addOrReplaceChild("leftear_r1", CubeListBuilder.create().texOffs(53, 67).addBox(-4.25F, -35.0F, -1.0F, 2.0F, 4.0F, 3.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(-2.5F, 33.0F, 0.0F, 0.0F, 0.0F, 0.2182F));
-
-        PartDefinition Hair = Head.addOrReplaceChild("Hair", CubeListBuilder.create().texOffs(16, 31).addBox(-2.0F, -34.75F, -4.0F, 4.0F, 1.0F, 8.0F, CubeDeformation.NONE)
-                .texOffs(32, 31).addBox(-4.0F, -34.5F, -3.5F, 8.0F, 1.0F, 4.0F, CubeDeformation.NONE)
-                .texOffs(10, 69).addBox(2.0F, -34.75F, 0.0F, 2.0F, 1.0F, 4.0F, CubeDeformation.NONE)
-                .texOffs(48, 16).addBox(2.5F, -34.0F, -3.0F, 2.0F, 3.0F, 7.0F, new CubeDeformation(0.025F))
-                .texOffs(24, 18).addBox(3.5F, -34.0F, -4.5F, 1.0F, 2.0F, 2.0F, CubeDeformation.NONE)
-                .texOffs(24, 4).addBox(-4.5F, -34.0F, -4.5F, 1.0F, 2.0F, 2.0F, CubeDeformation.NONE)
-                .texOffs(0, 64).addBox(2.5F, -31.0F, -1.0F, 2.0F, 2.0F, 5.0F, CubeDeformation.NONE)
-                .texOffs(32, 63).addBox(-4.5F, -31.0F, -1.0F, 2.0F, 2.0F, 5.0F, CubeDeformation.NONE)
-                .texOffs(45, 45).addBox(-4.5F, -34.0F, -3.0F, 2.0F, 3.0F, 7.0F, new CubeDeformation(0.025F))
-                .texOffs(68, 35).addBox(-4.0F, -34.75F, 0.0F, 2.0F, 1.0F, 4.0F, CubeDeformation.NONE)
-                .texOffs(56, 50).addBox(-4.0F, -34.0F, -4.5F, 8.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-                .texOffs(56, 43).addBox(-4.0F, -34.0F, 4.0F, 8.0F, 6.0F, 1.0F, CubeDeformation.NONE)
-                .texOffs(64, 10).addBox(-3.5F, -33.0F, -4.25F, 7.0F, 1.0F, 1.0F, CubeDeformation.NONE), PartPose.offset(0.0F, 25.5F, 0.0F));
-
-        PartDefinition leftupperantenna_r1 = Hair.addOrReplaceChild("leftupperantenna_r1", CubeListBuilder.create().texOffs(0, 32).addBox(2.0F, -29.0F, -25.5F, 1.0F, 3.0F, 1.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.6981F, 0.0873F, 0.0F));
-
-        PartDefinition leftlowerantenna_r1 = Hair.addOrReplaceChild("leftlowerantenna_r1", CubeListBuilder.create().texOffs(0, 0).addBox(1.5F, -34.0F, -14.0F, 2.0F, 3.0F, 2.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.3054F, 0.0873F, 0.0F));
-
-        PartDefinition rightupperantenna_r1 = Hair.addOrReplaceChild("rightupperantenna_r1", CubeListBuilder.create().texOffs(0, 16).addBox(-3.0F, -29.0F, -25.5F, 1.0F, 3.0F, 1.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.6981F, -0.0873F, 0.0F));
-
-        PartDefinition rightlowerantenna_r1 = Hair.addOrReplaceChild("rightlowerantenna_r1", CubeListBuilder.create().texOffs(16, 32).addBox(-3.5F, -34.0F, -14.0F, 2.0F, 3.0F, 2.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.3054F, -0.0873F, 0.0F));
 
         PartDefinition Torso = partdefinition.addOrReplaceChild("Torso", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -0.5F, -2.0F, 8.0F, 12.0F, 4.0F, CubeDeformation.NONE)
                 .texOffs(60, 0).addBox(-4.0F, -0.5F, 1.5F, 8.0F, 2.0F, 1.0F, CubeDeformation.NONE)
