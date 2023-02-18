18e161cc5b373aef3a9f05e234fb8e23b54fe0f7
Bijimio <mp39@tutanota.com>
2023-02-18 04:54:49 -0800

    Bijimio Experimental Fork V2.7.8

--- src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSharkFemale.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSharkFemale.java
@@ -2,12 +2,25 @@ package net.ltxprogrammer.changed.entity.beast;
 
 import net.ltxprogrammer.changed.entity.Gender;
 import net.ltxprogrammer.changed.entity.GenderedEntity;
+import net.ltxprogrammer.changed.entity.HairStyle;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class LatexSharkFemale extends AbstractLatexShark implements GenderedEntity {
     public LatexSharkFemale(EntityType<? extends LatexSharkFemale> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
+    }
+
+    @Override
+    public HairStyle getDefaultHairStyle() {
+        return HairStyle.LEGACY_FEMALE_RIGHT_BANG_S;
+    }
+
+    public @Nullable List<HairStyle> getValidHairStyles() {
+        return HairStyle.Collections.FEMALE_SHADED;
     }
 
     @Override
--- src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSharkMale.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexSharkMale.java
@@ -2,12 +2,25 @@ package net.ltxprogrammer.changed.entity.beast;
 
 import net.ltxprogrammer.changed.entity.Gender;
 import net.ltxprogrammer.changed.entity.GenderedEntity;
+import net.ltxprogrammer.changed.entity.HairStyle;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class LatexSharkMale extends AbstractLatexShark implements GenderedEntity {
     public LatexSharkMale(EntityType<? extends LatexSharkMale> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
+    }
+
+    @Override
+    public HairStyle getDefaultHairStyle() {
+        return HairStyle.MALE_NWE_S;
+    }
+
+    public @Nullable List<HairStyle> getValidHairStyles() {
+        return HairStyle.Collections.MALE_SHADED;
     }
 
     @Override
