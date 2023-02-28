e58d12cf91a4d6d9df8779838c1c0e67f2ea4072
LtxPgm <ltxprogrammer@outlook.com>
2023-02-28 16:37:48 -0600

    Added advancement for sleeping in a beehive
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedCriteriaTriggers.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedCriteriaTriggers.java
@@ -1,6 +1,7 @@
 package net.ltxprogrammer.changed.init;
 
 import net.ltxprogrammer.changed.advancements.critereon.AquaticBreatheTrigger;
+import net.ltxprogrammer.changed.advancements.critereon.BeehiveSleepTrigger;
 import net.ltxprogrammer.changed.advancements.critereon.TransfurTrigger;
 import net.ltxprogrammer.changed.advancements.critereon.WhiteLatexFuseTrigger;
 import net.minecraftforge.fml.common.Mod;
@@ -12,4 +13,5 @@ public class ChangedCriteriaTriggers {
     public static final TransfurTrigger TRANSFUR = register(new TransfurTrigger());
     public static final AquaticBreatheTrigger AQUATIC_BREATHE = register(new AquaticBreatheTrigger());
     public static final WhiteLatexFuseTrigger WHITE_LATEX_FUSE = register(new WhiteLatexFuseTrigger());
+    public static final BeehiveSleepTrigger BEEHIVE_SLEEP = register(new BeehiveSleepTrigger());
 }
