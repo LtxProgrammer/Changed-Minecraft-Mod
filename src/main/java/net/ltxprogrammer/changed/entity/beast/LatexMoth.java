5ce324e289e8bb7332ab6104e7d100b6ba35366d
bijimio <117211742+bijimio@users.noreply.github.com>
2023-03-02 09:28:37 -0800

    Added latex moth; gave TSC shield a recipe. (#331)
    * Bijimio Experimental Fork V3.1.9
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

---
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/LatexMoth.java
@@ -0,0 +1,49 @@
+package net.ltxprogrammer.changed.entity.beast;
+
+import net.ltxprogrammer.changed.entity.HairStyle;
+import net.ltxprogrammer.changed.entity.LatexEntity;
+import net.ltxprogrammer.changed.entity.LatexType;
+import net.ltxprogrammer.changed.entity.TransfurMode;
+import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LatexMoth extends LatexEntity {
+    public LatexMoth(EntityType<? extends LatexMoth> p_19870_, Level p_19871_) {
+        super(p_19870_, p_19871_);
+    }
+
+    @Override
+    public int getTicksRequiredToFreeze() { return 240; }
+
+    @Override
+    public HairStyle getDefaultHairStyle() {
+        return HairStyle.MALE_NWE;
+    }
+
+    @Override
+    public @Nullable List<HairStyle> getValidHairStyles() {
+        return HairStyle.Collections.MALE_NO_WOLF_EARS;
+    }
+
+    @Override
+    public ChangedParticles.Color3 getHairColor(int layer) {
+        return ChangedParticles.Color3.getColor("#a17c77");
+    }
+
+    @Override
+    public LatexType getLatexType() {
+        return LatexType.NEUTRAL;
+    }
+
+    @Override
+    public TransfurMode getTransfurMode() { return TransfurMode.REPLICATION; }
+
+    @Override
+    public ChangedParticles.Color3 getDripColor() {
+        return ChangedParticles.Color3.getColor("#fbe5bc");
+    }
+}
