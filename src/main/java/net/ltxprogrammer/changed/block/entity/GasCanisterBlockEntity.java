46493fc040cd0275b0fa1fc7c53ab71744d5afd4
LtxPgm <ltxprogrammer@outlook.com>
2023-03-03 18:38:57 -0600

    Gave gas canister "durability" that saves when placed
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

---
+++ src/main/java/net/ltxprogrammer/changed/block/entity/GasCanisterBlockEntity.java
@@ -0,0 +1,51 @@
+package net.ltxprogrammer.changed.block.entity;
+
+import net.ltxprogrammer.changed.entity.LatexType;
+import net.ltxprogrammer.changed.init.ChangedBlockEntities;
+import net.ltxprogrammer.changed.item.AbstractLatexBucket;
+import net.ltxprogrammer.changed.item.AbstractLatexGoo;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class GasCanisterBlockEntity extends BlockEntity {
+    private int usage = 0;
+
+    public GasCanisterBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ChangedBlockEntities.GAS_CANISTER.get(), blockPos, blockState);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putInt("Usage", usage);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("Usage")) usage = tag.getByte("Usage");
+    }
+
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    public CompoundTag getUpdateTag() {
+        CompoundTag tag = new CompoundTag();
+        tag.putInt("Usage", usage);
+        return tag;
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void setUsage(int usage) {
+        this.usage = usage;
+    }
+}
