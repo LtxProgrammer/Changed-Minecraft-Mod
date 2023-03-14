8571619255e14b9e0e44a5ab4f3496dbcbf49b19
bijimio <117211742+bijimio@users.noreply.github.com>
2023-03-14 10:53:33 -0700

    Fixed some blocks not being mineable. (#350)
    * Bijimio Experimental Fork V3.4.0
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/block/GasCanisterBlock.java
+++ src/main/java/net/ltxprogrammer/changed/block/GasCanisterBlock.java
@@ -29,7 +29,7 @@ public class GasCanisterBlock extends AbstractCustomShapeTallEntityBlock {
     public static final VoxelShape SHAPE_WHOLE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 28.0D, 12.0D);
 
     public GasCanisterBlock() {
-        super(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY).sound(SoundType.METAL).color(MaterialColor.WOOL).strength(2.0F, 6.0F));
+        super(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY).sound(SoundType.METAL).color(MaterialColor.WOOL).strength(1.0F, 5.0F));
     }
 
     @Override
