0b3187afa7d23a922b5827354a56e6b35ee166d5
bijimio <117211742+bijimio@users.noreply.github.com>
2023-03-17 15:29:04 -0700

    Improved block mining logic. (#352)
    * Bijimio Experimental Fork V3.4.3
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/block/AbstractCanBlock.java
+++ src/main/java/net/ltxprogrammer/changed/block/AbstractCanBlock.java
@@ -9,13 +9,14 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AbstractCanBlock extends AbstractCustomShapeBlock {
-    public static final VoxelShape SHAPE_WHOLE = Block.box(6.0D, 0.0D, 6.0D, 10.0D, 7.0D, 10.0D);
+    public static final VoxelShape SHAPE_WHOLE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 7.0D, 12.0D);
 
     public AbstractCanBlock() {
-        super(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.COPPER).strength(0.5F, 6.0F).noOcclusion());
+        super(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.COPPER).strength(0.5F).noOcclusion());
     }
 
     public boolean canSurvive(BlockState p_52783_, LevelReader p_52784_, BlockPos p_52785_) {
@@ -24,6 +25,11 @@ public class AbstractCanBlock extends AbstractCustomShapeBlock {
 
     @Override
     public VoxelShape getInteractionShape(BlockState p_60547_, BlockGetter p_60548_, BlockPos p_60549_) {
+        return SHAPE_WHOLE;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
         return SHAPE_WHOLE;
     }
 
