e410cbb82b8ce6cf7a627682d226922547e769c5
LtxPgm <ltxprogrammer@outlook.com>
2023-03-20 17:35:23 -0500

    Made emote particle track over entity
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/effect/particle/EmoteParticleOption.java
+++ src/main/java/net/ltxprogrammer/changed/effect/particle/EmoteParticleOption.java
@@ -3,33 +3,52 @@ package net.ltxprogrammer.changed.effect.particle;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.ltxprogrammer.changed.entity.Emote;
-import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.ltxprogrammer.changed.util.UniversalDist;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
 public class EmoteParticleOption implements ParticleOptions {
+    private static Entity entityOrException(int id) {
+        return UniversalDist.getLevel().getEntity(id);
+    }
+
     public static final Deserializer<EmoteParticleOption> DESERIALIZER = new Deserializer<>() {
         public EmoteParticleOption fromCommand(ParticleType<EmoteParticleOption> type, StringReader reader) throws CommandSyntaxException {
             reader.expect(' ');
-            return new EmoteParticleOption(type, Emote.values()[reader.readInt()]);
+            Emote emote = Emote.values()[reader.readInt()];
+            reader.expect(' ');
+            return new EmoteParticleOption(type, emote, entityOrException(reader.readInt()));
         }
 
         public EmoteParticleOption fromNetwork(ParticleType<EmoteParticleOption> type, FriendlyByteBuf buffer) {
-            return new EmoteParticleOption(type, Emote.values()[buffer.readInt()]);
+            return new EmoteParticleOption(type, Emote.values()[buffer.readInt()], entityOrException(buffer.readInt()));
         }
     };
     public static Codec<EmoteParticleOption> codec(ParticleType<EmoteParticleOption> type) {
-        return Emote.CODEC.xmap((emote) -> new EmoteParticleOption(type, emote), (option) -> option.emote);
+        return RecordCodecBuilder.create(builder -> {
+            return builder.group(
+                    Emote.CODEC.fieldOf("emote").forGetter(option -> option.emote),
+                    Codec.INT.fieldOf("entity").forGetter(option -> option.entity.getId())
+            ).apply(builder, (emote, id) -> {
+                return new EmoteParticleOption(type, emote, entityOrException(id));
+            });
+        });
     }
     private final ParticleType<EmoteParticleOption> type;
     private final Emote emote;
+    private final Entity entity;
 
-    public EmoteParticleOption(ParticleType<EmoteParticleOption> type, Emote emote) {
+    public EmoteParticleOption(ParticleType<EmoteParticleOption> type, Emote emote, Entity entity) {
         this.type = type;
         this.emote = emote;
+        this.entity = entity;
     }
 
     @Override
@@ -49,5 +68,9 @@ public class EmoteParticleOption implements ParticleOptions {
     @Override
     public String writeToString() {
         return Registry.PARTICLE_TYPE.getKey(this.getType()) + " " + emote.ordinal();
+    }
+
+    public Entity getEntity() {
+        return entity;
     }
 }
