98686f66d3394de3e496f3d51255d3ce6bb4f8ee
Bijimio <mp39@tutanota.com>
2023-03-23 22:27:44 -0700

    Bijimio Experimental Fork V3.6.6

--- src/main/java/net/ltxprogrammer/changed/entity/beast/boss/BehemothHandRight.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/boss/BehemothHandRight.java
@@ -3,9 +3,6 @@ package net.ltxprogrammer.changed.entity.beast.boss;
 import net.ltxprogrammer.changed.entity.HairStyle;
 import net.ltxprogrammer.changed.init.ChangedParticles;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
