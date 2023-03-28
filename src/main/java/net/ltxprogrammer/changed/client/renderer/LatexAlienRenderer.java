c1cbd0ec64b479842ba5353ab70ad34994938094
bijimio <117211742+bijimio@users.noreply.github.com>
2023-03-27 21:52:24 -0700

    Fixed alien tail; created alien armor model. (#370)
    * Bijimio Experimental Fork V3.7.3
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexAlienRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexAlienRenderer.java
@@ -3,15 +3,15 @@ package net.ltxprogrammer.changed.client.renderer;
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.EmissiveBodyLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexAlienModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexWolfModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexAlienModel;
 import net.ltxprogrammer.changed.entity.beast.LatexAlien;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexAlienRenderer extends LatexHumanoidRenderer<LatexAlien, LatexAlienModel, ArmorLatexWolfModel<LatexAlien>> {
+public class LatexAlienRenderer extends LatexHumanoidRenderer<LatexAlien, LatexAlienModel, ArmorLatexAlienModel<LatexAlien>> {
     public LatexAlienRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexAlienModel(context.bakeLayer(LatexAlienModel.LAYER_LOCATION)),
-                ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexAlienModel::new, ArmorLatexAlienModel.INNER_ARMOR, ArmorLatexAlienModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new EmissiveBodyLayer<>(this, Changed.modResource("textures/latex_alien_emissive.png")));
     }
 
