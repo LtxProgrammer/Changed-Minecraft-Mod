cf1fce463a8ca3880a9a8f3952969eb7bb677ab9
LtxPgm <ltxprogrammer@outlook.com>
2023-04-25 22:55:39 -0500

    Increased redundancy check for latexed events
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/EventHandlerClient.java
+++ src/main/java/net/ltxprogrammer/changed/client/EventHandlerClient.java
@@ -91,7 +91,7 @@ public class EventHandlerClient {
 
         @SubscribeEvent
         public static void onChangedVariant(ProcessTransfur.EntityVariantAssigned.ChangedVariant event) {
-            if (event.oldVariant != null || event.newVariant == null || event.livingEntity.tickCount < 5)
+            if (event.oldVariant != null || event.newVariant == null || event.livingEntity.tickCount < 20)
                 return; // Only do effect if player was human
 
             event.livingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 10 * 20, 1, false, false));
