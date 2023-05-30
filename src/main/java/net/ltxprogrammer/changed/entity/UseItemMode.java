216d56c8d9afe87b130dcde5ced0a879658af43b
LtxPgm <ltxprogrammer@outlook.com>
2023-05-29 23:39:08 -0500

    Replaced disabled hotbar render, with transparent hotbar.
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/entity/UseItemMode.java
+++ src/main/java/net/ltxprogrammer/changed/entity/UseItemMode.java
@@ -8,26 +8,28 @@ public enum UseItemMode implements IExtensibleEnum {
     /**
      * The variant can use items like a normal player.
      */
-    NORMAL(true, true, true, true),
+    NORMAL(true, true, true, true, true),
     /**
      * The variant can only hold one item out, in their mouth.
      */
-    MOUTH(true, true, false, true),
+    MOUTH(true, true, false, true, false),
     /**
      * The variant can't hold any items out, nor use them.
      */
-    NONE(false, false, false, false);
+    NONE(false, false, false, false, false);
 
     public final boolean showHotbar;
     public final boolean holdMainHand;
     public final boolean holdOffHand;
     public final boolean interactWithBlocks;
+    public final boolean breakBlocks;
 
-    UseItemMode(boolean showHotbar, boolean holdMainHand, boolean holdOffHand, boolean interactWithBlocks) {
+    UseItemMode(boolean showHotbar, boolean holdMainHand, boolean holdOffHand, boolean interactWithBlocks, boolean breakBlocks) {
         this.showHotbar = showHotbar;
         this.holdMainHand = holdMainHand;
         this.holdOffHand = holdOffHand;
         this.interactWithBlocks = interactWithBlocks;
+        this.breakBlocks = breakBlocks;
     }
 
     public boolean canUseHand(InteractionHand hand) {
@@ -37,7 +39,7 @@ public enum UseItemMode implements IExtensibleEnum {
         };
     }
 
-    public static UseItemMode create(boolean showHotbar, boolean holdMainHand, boolean holdOffHand, boolean interactWithBlocks) {
+    public static UseItemMode create(String name, boolean showHotbar, boolean holdMainHand, boolean holdOffHand, boolean interactWithBlocks, boolean breakBlocks) {
         throw new NotImplementedException("Not extended");
     }
 }
