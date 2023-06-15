7232ac4d3b627aabaa9e84137a67665c73de69a5
LtxPgm <ltxprogrammer@outlook.com>
2023-06-15 17:03:32 -0500

    Added tameability to DL latex pup. They grow up tamed too. Perhaps make a way to have tamed latex do keep-con transfur upon owner request?
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/entity/beast/AbstractDarkLatexEntity.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/AbstractDarkLatexEntity.java
@@ -1,16 +1,85 @@
 package net.ltxprogrammer.changed.entity.beast;
 
 import net.ltxprogrammer.changed.entity.LatexType;
+import net.ltxprogrammer.changed.entity.ai.LatexFollowOwnerGoal;
+import net.ltxprogrammer.changed.init.ChangedCriteriaTriggers;
+import net.ltxprogrammer.changed.init.ChangedItems;
 import net.ltxprogrammer.changed.init.ChangedParticles;
+import net.minecraft.Util;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.OldUsersConverter;
 import net.minecraft.util.Mth;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.OwnableEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.scores.Team;
+import org.jetbrains.annotations.Nullable;
 
-public abstract class AbstractDarkLatexEntity extends AbstractLatexWolf implements DarkLatexEntity {
+import java.util.Optional;
+import java.util.UUID;
+
+public abstract class AbstractDarkLatexEntity extends AbstractLatexWolf implements DarkLatexEntity, OwnableEntity {
+    protected static final EntityDataAccessor<Byte> DATA_FLAGS_ID = SynchedEntityData.defineId(AbstractDarkLatexEntity.class, EntityDataSerializers.BYTE);
+    protected static final EntityDataAccessor<Optional<UUID>> DATA_OWNERUUID_ID = SynchedEntityData.defineId(AbstractDarkLatexEntity.class, EntityDataSerializers.OPTIONAL_UUID);
 
     public AbstractDarkLatexEntity(EntityType<? extends AbstractLatexWolf> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(6, new LatexFollowOwnerGoal<>(this, 1.0D, 10.0F, 2.0F, false));
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(DATA_FLAGS_ID, (byte)0);
+        this.entityData.define(DATA_OWNERUUID_ID, Optional.empty());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+
+        UUID uuid;
+        if (tag.hasUUID("Owner")) {
+            uuid = tag.getUUID("Owner");
+        } else {
+            String s = tag.getString("Owner");
+            uuid = OldUsersConverter.convertMobOwnerIfNecessary(this.getServer(), s);
+        }
+
+        if (uuid != null) {
+            try {
+                this.setOwnerUUID(uuid);
+                this.setTame(true);
+            } catch (Throwable throwable) {
+                this.setTame(false);
+            }
+        }
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        super.addAdditionalSaveData(tag);
+
+        if (this.getOwnerUUID() != null) {
+            tag.putUUID("Owner", this.getOwnerUUID());
+        }
     }
 
     public boolean isMaskless() {
@@ -29,6 +98,9 @@ public abstract class AbstractDarkLatexEntity extends AbstractLatexWolf implemen
 
     @Override
     protected boolean targetSelectorTest(LivingEntity livingEntity) {
+        if (livingEntity == this.getOwner())
+            return false;
+
         if (!this.isMaskless()) {// Check if masked DL can see entity
             if (livingEntity.distanceToSqr(this) <= 1.0)
                 return super.targetSelectorTest(livingEntity);
@@ -42,5 +114,132 @@ public abstract class AbstractDarkLatexEntity extends AbstractLatexWolf implemen
         }
 
         return super.targetSelectorTest(livingEntity);
+    }
+
+    @Nullable
+    @Override
+    public UUID getOwnerUUID() {
+        return this.entityData.get(DATA_OWNERUUID_ID).orElse(null);
+    }
+
+    public boolean canBeLeashed(Player player) {
+        return !this.isLeashed();
+    }
+
+    protected void spawnTamingParticles(boolean success) {
+        ParticleOptions particleoptions = ParticleTypes.HEART;
+        if (!success) {
+            particleoptions = ParticleTypes.SMOKE;
+        }
+
+        for(int i = 0; i < 7; ++i) {
+            double d0 = this.random.nextGaussian() * 0.02D;
+            double d1 = this.random.nextGaussian() * 0.02D;
+            double d2 = this.random.nextGaussian() * 0.02D;
+            this.level.addParticle(particleoptions, this.getRandomX(1.0D), this.getRandomY() + 0.5D, this.getRandomZ(1.0D), d0, d1, d2);
+        }
+
+    }
+
+    public void handleEntityEvent(byte event) {
+        if (event == 7) {
+            this.spawnTamingParticles(true);
+        } else if (event == 6) {
+            this.spawnTamingParticles(false);
+        } else {
+            super.handleEntityEvent(event);
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public LivingEntity getOwner() {
+        try {
+            UUID uuid = this.getOwnerUUID();
+            return uuid == null ? null : this.level.getPlayerByUUID(uuid);
+        } catch (IllegalArgumentException illegalargumentexception) {
+            return null;
+        }
+    }
+
+    public void setOwnerUUID(@Nullable UUID uuid) {
+        this.entityData.set(DATA_OWNERUUID_ID, Optional.ofNullable(uuid));
+    }
+
+    public void tame(Player player) {
+        this.setTame(true);
+        this.setOwnerUUID(player.getUUID());
+        if (player instanceof ServerPlayer) {
+            ChangedCriteriaTriggers.TAME_LATEX.trigger((ServerPlayer)player, this);
+        }
+
+    }
+
+    public boolean isTame() {
+        return (this.entityData.get(DATA_FLAGS_ID) & 4) != 0;
+    }
+
+    public void setTame(boolean tame) {
+        byte b0 = this.entityData.get(DATA_FLAGS_ID);
+        if (tame) {
+            this.entityData.set(DATA_FLAGS_ID, (byte)(b0 | 4));
+        } else {
+            this.entityData.set(DATA_FLAGS_ID, (byte)(b0 & -5));
+        }
+
+        this.reassessTameGoals();
+    }
+
+    protected void reassessTameGoals() {
+    }
+
+    public boolean isOwnedBy(LivingEntity entity) {
+        return entity == this.getOwner();
+    }
+
+    public boolean canAttack(LivingEntity entity) {
+        return !this.isOwnedBy(entity) && super.canAttack(entity);
+    }
+
+    public Team getTeam() {
+        if (this.isTame()) {
+            LivingEntity livingentity = this.getOwner();
+            if (livingentity != null) {
+                return livingentity.getTeam();
+            }
+        }
+
+        return super.getTeam();
+    }
+
+    public boolean isAlliedTo(Entity entity) {
+        if (this.isTame()) {
+            LivingEntity livingentity = this.getOwner();
+            if (entity == livingentity) {
+                return true;
+            }
+
+            if (livingentity != null) {
+                return livingentity.isAlliedTo(entity);
+            }
+        }
+
+        return super.isAlliedTo(entity);
+    }
+
+    public void die(DamageSource source) {
+        // FORGE: Super moved to top so that death message would be cancelled properly
+        net.minecraft.network.chat.Component deathMessage = this.getCombatTracker().getDeathMessage();
+        super.die(source);
+
+        if (this.dead)
+            if (!this.level.isClientSide && this.level.getGameRules().getBoolean(GameRules.RULE_SHOWDEATHMESSAGES) && this.getOwner() instanceof ServerPlayer) {
+                this.getOwner().sendMessage(deathMessage, Util.NIL_UUID);
+            }
+    }
+
+    protected boolean isTameItem(ItemStack stack) {
+        return stack.is(ChangedItems.WHITE_LATEX_GOO.get()) || stack.is(ChangedItems.ORANGE.get());
     }
 }
--- src/main/java/net/ltxprogrammer/changed/entity/beast/DarkLatexPup.java
+++ src/main/java/net/ltxprogrammer/changed/entity/beast/DarkLatexPup.java
@@ -1,18 +1,39 @@
 package net.ltxprogrammer.changed.entity.beast;
 
 import net.ltxprogrammer.changed.entity.HairStyle;
+import net.ltxprogrammer.changed.entity.LatexEntity;
 import net.ltxprogrammer.changed.entity.TransfurMode;
 import net.ltxprogrammer.changed.entity.variant.LatexVariant;
+import net.ltxprogrammer.changed.init.ChangedCriteriaTriggers;
+import net.ltxprogrammer.changed.init.ChangedItems;
 import net.ltxprogrammer.changed.init.ChangedParticles;
 import net.ltxprogrammer.changed.init.ChangedSounds;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
+import net.minecraft.Util;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.OldUsersConverter;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.scores.Team;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class DarkLatexPup extends AbstractDarkLatexEntity {
     protected static final int MAX_AGE = 72000;
@@ -83,13 +104,47 @@ public class DarkLatexPup extends AbstractDarkLatexEntity {
             if (wolf != null) {
                 wolf.moveTo(this.getX(), this.getY(), this.getZ(), this.getYRot(), this.getXRot());
                 level.addFreshEntity(wolf);
+                ChangedSounds.broadcastSound(this, ChangedSounds.POISON, 1.0f, 1.0f);
+                applyCustomizeToAged((AbstractDarkLatexEntity)wolf);
             }
             this.discard();
         }
     }
 
+    protected void applyCustomizeToAged(AbstractDarkLatexEntity aged) {
+        aged.setOwnerUUID(this.getOwnerUUID());
+    }
+
     @Override
     protected boolean targetSelectorTest(LivingEntity livingEntity) {
         return false; // TODO remove when pup puddle is implemented.
+    }
+
+    @Override
+    protected InteractionResult mobInteract(Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (this.level.isClientSide) {
+            boolean flag = this.isOwnedBy(player) || this.isTame() || this.isTameItem(itemstack) && !this.isTame();
+            return flag ? InteractionResult.CONSUME : InteractionResult.PASS;
+        } else {
+            if (!this.isTame() && this.isTameItem(itemstack)) {
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                if (this.random.nextInt(3) == 0) {
+                    this.tame(player);
+                    this.navigation.stop();
+                    this.setTarget(null);
+                    this.level.broadcastEntityEvent(this, (byte)7);
+                } else {
+                    this.level.broadcastEntityEvent(this, (byte)6);
+                }
+
+                return InteractionResult.SUCCESS;
+            }
+
+            return super.mobInteract(player, hand);
+        }
     }
 }
