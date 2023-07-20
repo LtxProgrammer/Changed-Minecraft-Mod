bff927534be04c20f8402c23e0e834e001c344b6
LtxPgm <ltxprogrammer@outlook.com>
2023-07-20 12:04:36 -0500

    Moved loot tables to code based reference
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedStructures.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedStructures.java
@@ -3,6 +3,7 @@ package net.ltxprogrammer.changed.init;
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.world.features.structures.Beehive;
 import net.ltxprogrammer.changed.world.features.structures.DecayedLab;
+import net.ltxprogrammer.changed.world.features.structures.LootTables;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
@@ -30,23 +31,23 @@ public class ChangedStructures {
             DeferredRegister.create(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.key(), Changed.MODID);
 
     public static final Holder<ConfiguredStructureFeature<?, ?>> BEEHIVE1 = registerBeehive("beehive1");
-    public static final Holder<ConfiguredStructureFeature<?, ?>> AQUATIC1 = registerLab("aquatic1", Changed.modResource("chests/decayed_lab_aqua"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> AQUATIC2 = registerLab("aquatic2", Changed.modResource("chests/decayed_lab_aqua"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> HUMAN_RESEARCH_LAB1 = registerLab("human_research_lab1", Changed.modResource("chests/decayed_lab_treatment"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> LASER_LAB1 = registerLab("laser_lab1", Changed.modResource("chests/low_tier_lab"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> OFFICE_AREA1 = registerLab("office_area1", Changed.modResource("chests/decayed_lab_dl"), ChangedTags.Biomes.HAS_DARK_DECAYED_LABS);
-    public static final Holder<ConfiguredStructureFeature<?, ?>> OFFICE_AREA2 = registerLab("office_area2", Changed.modResource("chests/decayed_lab_dl"), ChangedTags.Biomes.HAS_DARK_DECAYED_LABS);
-    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER1 = registerLab("research_tower1", Changed.modResource("chests/decayed_lab_origin"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER2 = registerLab("research_tower2", Changed.modResource("chests/decayed_lab_origin"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER3 = registerLab("research_tower3", Changed.modResource("chests/decayed_lab_origin"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER4 = registerLab("research_tower4", Changed.modResource("chests/decayed_lab_origin"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_1 = registerLab("treatment_1", Changed.modResource("chests/high_tier_lab"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_2 = registerLab("treatment_2", Changed.modResource("chests/high_tier_lab"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_3 = registerLab("treatment_3", Changed.modResource("chests/high_tier_lab"));
+    public static final Holder<ConfiguredStructureFeature<?, ?>> AQUATIC1 = registerLab("aquatic1", LootTables.DECAYED_LAB_AQUA);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> AQUATIC2 = registerLab("aquatic2", LootTables.DECAYED_LAB_AQUA);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> HUMAN_RESEARCH_LAB1 = registerLab("human_research_lab1",LootTables.DECAYED_LAB_TREATMENT);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> LASER_LAB1 = registerLab("laser_lab1", LootTables.LOW_TIER_LAB);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> OFFICE_AREA1 = registerLab("office_area1", LootTables.DECAYED_LAB_DL, ChangedTags.Biomes.HAS_DARK_DECAYED_LABS);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> OFFICE_AREA2 = registerLab("office_area2", LootTables.DECAYED_LAB_DL, ChangedTags.Biomes.HAS_DARK_DECAYED_LABS);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER1 = registerLab("research_tower1", LootTables.DECAYED_LAB_ORIGIN);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER2 = registerLab("research_tower2", LootTables.DECAYED_LAB_ORIGIN);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER3 = registerLab("research_tower3", LootTables.DECAYED_LAB_ORIGIN);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> RESEARCH_TOWER4 = registerLab("research_tower4", LootTables.DECAYED_LAB_ORIGIN);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_1 = registerLab("treatment_1", LootTables.HIGH_TIER_LAB);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_2 = registerLab("treatment_2", LootTables.HIGH_TIER_LAB);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> TREATMENT_3 = registerLab("treatment_3", LootTables.HIGH_TIER_LAB);
     // TODO replace with procedural gen facility that stretches underground
-    //public static final Holder<ConfiguredStructureFeature<?, ?>> UNDERGROUND_DL_LAB1 = registerLab("underground_dl_lab1", Changed.modResource("chests/decayed_lab_dl"));
-    public static final Holder<ConfiguredStructureFeature<?, ?>> WHITE_LATEX_LAB1 = registerLab("white_latex_lab1", Changed.modResource("chests/decayed_lab_wl"), ChangedTags.Biomes.HAS_WHITE_DECAYED_LABS);
-    public static final Holder<ConfiguredStructureFeature<?, ?>> WHITE_LATEX_LAB2 = registerLab("white_latex_lab2", Changed.modResource("chests/decayed_lab_wl"), ChangedTags.Biomes.HAS_WHITE_DECAYED_LABS);
+    //public static final Holder<ConfiguredStructureFeature<?, ?>> UNDERGROUND_DL_LAB1 = registerLab("underground_dl_lab1", LootTables.DECAYED_LAB_DL);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> WHITE_LATEX_LAB1 = registerLab("white_latex_lab1", LootTables.DECAYED_LAB_WL, ChangedTags.Biomes.HAS_WHITE_DECAYED_LABS);
+    public static final Holder<ConfiguredStructureFeature<?, ?>> WHITE_LATEX_LAB2 = registerLab("white_latex_lab2", LootTables.DECAYED_LAB_WL, ChangedTags.Biomes.HAS_WHITE_DECAYED_LABS);
 
     private static Holder<ConfiguredStructureFeature<?, ?>> registerLab(String nbt, ResourceLocation lootTable) {
         return registerLab(nbt, lootTable, ChangedTags.Biomes.HAS_DECAYED_LABS);
---
+++ src/main/java/net/ltxprogrammer/changed/world/features/structures/LootTables.java
@@ -0,0 +1,15 @@
+package net.ltxprogrammer.changed.world.features.structures;
+
+import net.ltxprogrammer.changed.Changed;
+import net.minecraft.resources.ResourceLocation;
+
+public class LootTables {
+    public static final ResourceLocation DECAYED_LAB_AQUA = Changed.modResource("chests/decayed_lab_aqua");
+    public static final ResourceLocation DECAYED_LAB_DL = Changed.modResource("chests/decayed_lab_dl");
+    public static final ResourceLocation DECAYED_LAB_DLD = Changed.modResource("chests/decayed_lab_dld");
+    public static final ResourceLocation DECAYED_LAB_ORIGIN = Changed.modResource("chests/decayed_lab_origin");
+    public static final ResourceLocation DECAYED_LAB_TREATMENT = Changed.modResource("chests/decayed_lab_treatment");
+    public static final ResourceLocation DECAYED_LAB_WL = Changed.modResource("chests/decayed_lab_wl");
+    public static final ResourceLocation HIGH_TIER_LAB = Changed.modResource("chests/high_tier_lab");
+    public static final ResourceLocation LOW_TIER_LAB = Changed.modResource("chests/low_tier_lab");
+}
