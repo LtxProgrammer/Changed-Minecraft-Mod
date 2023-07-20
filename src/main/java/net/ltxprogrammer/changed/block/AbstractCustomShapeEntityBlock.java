52ba72a275491b5340bd0572a5cbe0d8435d3d76
LtxPgm <ltxprogrammer@outlook.com>
2023-07-20 10:14:58 -0500

    Fixed rotation and mirroring on abstract custom shape blocks
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/block/AbstractCustomShapeEntityBlock.java
+++ src/main/java/net/ltxprogrammer/changed/block/AbstractCustomShapeEntityBlock.java
@@ -3,9 +3,7 @@ package net.ltxprogrammer.changed.block;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -71,5 +69,13 @@ public abstract class AbstractCustomShapeEntityBlock extends BaseEntityBlock {
                     newShape.get() == null ? Shapes.empty() : newShape.get()));
         });
         return newShape.get();
+    }
+
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 }
