54e9dce45f1b60f22e93ae12357cd0196f6c85eb
LtxPgm <ltxprogrammer@outlook.com>
2023-07-31 16:04:49 -0500

    Remodeled male manta ray
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexMantaRayMaleRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexMantaRayMaleRenderer.java
@@ -2,8 +2,10 @@ package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.model.LatexMantaRayMaleModel;
+import net.ltxprogrammer.changed.client.renderer.model.LatexSharkModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexSharkModel;
 import net.ltxprogrammer.changed.entity.beast.LatexMantaRayMale;
+import net.ltxprogrammer.changed.entity.beast.LatexShark;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -16,5 +18,17 @@ public class LatexMantaRayMaleRenderer extends LatexHumanoidRenderer<LatexMantaR
     @Override
     public ResourceLocation getTextureLocation(LatexMantaRayMale p_114482_) {
         return Changed.modResource("textures/latex_manta_ray_male.png");
+    }
+
+    public static class Remodel extends LatexHumanoidRenderer<LatexMantaRayMale, LatexMantaRayMaleModel.Remodel, ArmorLatexSharkModel.RemodelMale<LatexMantaRayMale>> {
+        public Remodel(EntityRendererProvider.Context context) {
+            super(context, new LatexMantaRayMaleModel.Remodel(context.bakeLayer(LatexMantaRayMaleModel.LAYER_LOCATION)),
+                    ArmorLatexSharkModel.RemodelMale::new, ArmorLatexSharkModel.RemodelMale.INNER_ARMOR, ArmorLatexSharkModel.RemodelMale.OUTER_ARMOR, 0.5f);
+        }
+
+        @Override
+        public ResourceLocation getTextureLocation(LatexMantaRayMale p_114482_) {
+            return Changed.modResource("textures/remodel/latex_manta_ray_male.png");
+        }
     }
 }
