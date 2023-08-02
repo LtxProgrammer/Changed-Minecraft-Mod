2fb7733e626388c2c7b27249d0ed51cd8ce09339
LtxPgm <ltxprogrammer@outlook.com>
2023-08-02 15:20:13 -0500

    Remodeled siren
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSirenRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSirenRenderer.java
@@ -3,6 +3,7 @@ package net.ltxprogrammer.changed.client.renderer;
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.model.LatexSirenModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorAbdomenModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorMermaidSharkAbdomenModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorUpperBodyModel;
 import net.ltxprogrammer.changed.entity.beast.LatexSiren;
 import net.ltxprogrammer.changed.item.AbdomenArmor;
@@ -20,5 +21,19 @@ public class LatexSirenRenderer extends LatexHumanoidRenderer<LatexSiren, LatexS
     @Override
     public ResourceLocation getTextureLocation(LatexSiren p_114482_) {
         return Changed.modResource("textures/latex_siren.png");
+    }
+
+    public static class Remodel extends LatexHumanoidRenderer<LatexSiren, LatexSirenModel.Remodel, ArmorUpperBodyModel.RemodelFemale<LatexSiren>> {
+        public Remodel(EntityRendererProvider.Context context) {
+            super(context, new LatexSirenModel.Remodel(context.bakeLayer(LatexSirenModel.LAYER_LOCATION)),
+                    ArmorUpperBodyModel.RemodelFemale::new, ArmorUpperBodyModel.RemodelFemale.INNER_ARMOR, ArmorUpperBodyModel.RemodelFemale.OUTER_ARMOR,
+                    ArmorMermaidSharkAbdomenModel.Remodel::new, ArmorMermaidSharkAbdomenModel.Remodel.INNER_ARMOR, ArmorMermaidSharkAbdomenModel.Remodel.OUTER_ARMOR,
+                    AbdomenArmor::useAbdomenModel, AbdomenArmor::useInnerAbdomenModel, 0.5f);
+        }
+
+        @Override
+        public ResourceLocation getTextureLocation(LatexSiren p_114482_) {
+            return Changed.modResource("textures/remodel/latex_mermaid_shark_female.png");
+        }
     }
 }
