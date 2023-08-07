646eba18e4a4fb2b1e758a3876aabd1a02a33fa5
LtxPgm <ltxprogrammer@outlook.com>
2023-08-07 15:13:03 -0500

    Fixed #492
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/mixin/compatibility/Rubidium/BlockRenderPassManagerMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/Rubidium/BlockRenderPassManagerMixin.java
@@ -4,6 +4,7 @@ import it.unimi.dsi.fastutil.objects.Reference2IntArrayMap;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
 import net.ltxprogrammer.changed.client.LatexCoveredBlocks;
+import net.ltxprogrammer.changed.extension.rubidium.ChangedBlockRenderPass;
 import net.minecraft.client.renderer.RenderType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = BlockRenderPassManager.class, remap = false)
 public abstract class BlockRenderPassManagerMixin {
@@ -19,10 +21,10 @@ public abstract class BlockRenderPassManagerMixin {
     @Inject(method = "addMapping", at = @At("RETURN"))
     private void changedRenderMappings(RenderType layer, BlockRenderPass type, CallbackInfo ci) {
         if (layer == RenderType.solid())
-            this.mappingsId.put(LatexCoveredBlocks.latexSolid(), BlockRenderPass.SOLID.ordinal());
+            this.mappingsId.put(LatexCoveredBlocks.latexSolid(), ChangedBlockRenderPass.LATEX_SOLID.ordinal());
         else if (layer == RenderType.cutoutMipped())
-            this.mappingsId.put(LatexCoveredBlocks.latexCutoutMipped(), BlockRenderPass.CUTOUT_MIPPED.ordinal());
+            this.mappingsId.put(LatexCoveredBlocks.latexCutoutMipped(), ChangedBlockRenderPass.LATEX_CUTOUT_MIPPED.ordinal());
         else if (layer == RenderType.cutout())
-            this.mappingsId.put(LatexCoveredBlocks.latexCutout(), BlockRenderPass.CUTOUT.ordinal());
+            this.mappingsId.put(LatexCoveredBlocks.latexCutout(), ChangedBlockRenderPass.LATEX_CUTOUT.ordinal());
     }
 }
