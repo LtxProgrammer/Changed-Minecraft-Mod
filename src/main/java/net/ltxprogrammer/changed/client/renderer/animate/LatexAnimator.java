e116a94fb98657706e58dae15884c9022d1334f8
LtxPgm <ltxprogrammer@outlook.com>
2023-09-19 12:23:35 -0500

    Switched from null default model to property model for #525
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/LatexAnimator.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/LatexAnimator.java
@@ -6,6 +6,7 @@ import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.HumanoidArm;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,6 +22,7 @@ public class LatexAnimator<T extends LatexEntity, M extends EntityModel<T>> {
     public float armLength = 12.0f;
     public float legLength = 12.0f;
 
+    private final HumanoidModel<?> propertyModel;
     private final Map<Integer, Runnable> setupHandsRunnable = new HashMap<>();
     private final EnumMap<AnimateStage, List<Animator<T, M>>> animators = new EnumMap<>(AnimateStage.class);
     public HumanoidModel.ArmPose leftArmPose = HumanoidModel.ArmPose.EMPTY;
@@ -30,6 +32,45 @@ public class LatexAnimator<T extends LatexEntity, M extends EntityModel<T>> {
 
     public LatexAnimator(M entityModel) {
         this.entityModel = entityModel;
+        this.propertyModel = new HumanoidModel(new ModelPart(Collections.emptyList(),
+                Map.of("head", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "hat", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "body",
+                        new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_arm",
+                        new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_leg",
+                        new ModelPart(Collections.emptyList(), Collections.emptyMap()))));
+    }
+
+    /**
+     * This function is only for property purposes, and not to be directly rendered
+     * @return A HumanoidModel approximating the current latex model
+     */
+    public HumanoidModel<?> getPropertyModel(EquipmentSlot slot) {
+        propertyModel.leftArmPose = leftArmPose;
+        propertyModel.rightArmPose = rightArmPose;
+        propertyModel.crouching = crouching;
+        propertyModel.swimAmount = swimAmount;
+        propertyModel.attackTime = entityModel.attackTime;
+        propertyModel.riding = entityModel.riding;
+        propertyModel.young = entityModel.young;
+        propertyModel.setAllVisible(false);
+        switch (slot) {
+            case HEAD:
+                propertyModel.head.visible = true;
+                propertyModel.hat.visible = true;
+                break;
+            case CHEST:
+                propertyModel.body.visible = true;
+                propertyModel.leftArm.visible = true;
+                propertyModel.rightArm.visible = true;
+                break;
+            case LEGS:
+                propertyModel.body.visible = true;
+            case FEET:
+                propertyModel.leftLeg.visible = true;
+                propertyModel.rightLeg.visible = true;
+                break;
+        }
+
+        return propertyModel;
     }
 
     public static <T extends LatexEntity, M extends EntityModel<T>> LatexAnimator<T, M> of(M entityModel) {
