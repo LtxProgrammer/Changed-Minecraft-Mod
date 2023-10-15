a4618832dbdebfb7ac0c2494f1098f61632568ec
LtxPgm <ltxprogrammer@outlook.com>
2023-10-14 22:58:50 -0500

    Improved quick equipping restrictions with invalid armor items (trying to wear pants as a legless form)
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/mixin/item/ItemStackMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/item/ItemStackMixin.java
@@ -1,7 +1,13 @@
 package net.ltxprogrammer.changed.mixin.item;
 
+import net.ltxprogrammer.changed.process.ProcessTransfur;
+import net.ltxprogrammer.changed.util.EntityUtil;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.extensions.IForgeItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.function.Supplier;
 
 @Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
+public abstract class ItemStackMixin implements IForgeItemStack {
     private Supplier<? extends Item> itemSupplier;
 
     @Inject(method = "getItem", at = @At("HEAD"), cancellable = true)
@@ -23,5 +29,15 @@ public abstract class ItemStackMixin {
     public void toString(CallbackInfoReturnable<String> ci) {
         if (itemSupplier != null)
             ci.setReturnValue(new ItemStack(itemSupplier.get()).toString());
+    }
+
+    @Override
+    public boolean canEquip(EquipmentSlot armorType, Entity entity) {
+        Player player = EntityUtil.playerOrNull(entity);
+        return ProcessTransfur.ifPlayerLatex(player, variant -> {
+            if ((Object)this instanceof ItemStack stack)
+                return variant.canWear(player, stack);
+            return IForgeItemStack.super.canEquip(armorType, entity);
+        }, () -> IForgeItemStack.super.canEquip(armorType, entity));
     }
 }
