9c1c54f422ad06c3a7da364accf8d18d396c0035
LtxPgm <ltxprogrammer@outlook.com>
2023-11-06 16:26:08 -0600

    Adjusted generation for new glu block properties
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityCorridorSection.java
+++ src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityCorridorSection.java
@@ -5,13 +5,14 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.random.WeightedEntry;
 import net.minecraft.util.random.WeightedRandomList;
 
-public class FacilityStaircase extends FacilitySinglePiece {
+public class FacilityCorridorSection extends FacilitySinglePiece {
     private static final WeightedRandomList<WeightedEntry.Wrapper<PieceType>> VALID_NEIGHBORS = WeightedRandomList.create(
-            WeightedEntry.wrap(PieceType.STAIRCASE, 2),
-            WeightedEntry.wrap(PieceType.CORRIDOR, 1));
+            WeightedEntry.wrap(PieceType.STAIRCASE_START, 1),
+            WeightedEntry.wrap(PieceType.CORRIDOR, 5),
+            WeightedEntry.wrap(PieceType.ROOM, 3));
 
-    public FacilityStaircase(Zone zone, ResourceLocation templateName) {
-        super(PieceType.STAIRCASE, zone, templateName, LootTables.LOW_TIER_LAB);
+    public FacilityCorridorSection(Zone zone, ResourceLocation templateName) {
+        super(PieceType.STAIRCASE_SECTION, zone, templateName, LootTables.LOW_TIER_LAB);
     }
 
     @Override
--- src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityStaircaseStart.java
+++ src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityStaircaseStart.java
@@ -5,11 +5,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.random.WeightedEntry;
 import net.minecraft.util.random.WeightedRandomList;
 
-public class FacilityEntrance extends FacilitySinglePiece {
-    private static final WeightedRandomList<WeightedEntry.Wrapper<PieceType>> VALID_NEIGHBORS = WeightedRandomList.create(WeightedEntry.wrap(PieceType.STAIRCASE_START, 1));
+public class FacilityStaircaseStart extends FacilitySinglePiece {
+    private static final WeightedRandomList<WeightedEntry.Wrapper<PieceType>> VALID_NEIGHBORS = WeightedRandomList.create(
+            WeightedEntry.wrap(PieceType.STAIRCASE_SECTION, 1));
 
-    public FacilityEntrance(Zone zone, ResourceLocation templateName) {
-        super(PieceType.ENTRANCE, zone, templateName, LootTables.LOW_TIER_LAB);
+    public FacilityStaircaseStart(Zone zone, ResourceLocation templateName) {
+        super(PieceType.STAIRCASE_SECTION, zone, templateName, LootTables.LOW_TIER_LAB);
     }
 
     @Override
