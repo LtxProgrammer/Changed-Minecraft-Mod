f7e050dd797208b3edde81d1c08c656da533f5e0
LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>
2023-11-30 12:03:15 -0600

    Merge upstream (#590)
    * Modeled Unified Latex Yuin, Textured Unified Organic Light Wolf. (#585)
    
    * Bijimio Experimental Fork V5.1.1 | Modeled Unified Male Light Latex Wolf
    
    * Bijimio Experimental Fork V5.1.2 | Textured Organic Light Wolf, Tweaked Male Light Latex Texture.
    
    * Bijimio Experimental Fork V5.1.3 | Modeled Unified Latex Yuin, Added Joints to Deer Tail.
    
    * Bijimio Experimental Fork V5.1.4 | Made white latex visual overlay less harsh.
    
    * Made protection enchantment reduce TF progress to a lesser extent
    
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>
    
    * Changed advancements tab background as requested in #586
    
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>
    
    * Added crouch detection to new latex particles
    
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>
    
    ---------
    
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>
    Co-authored-by: bijimio <117211742+bijimio@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/client/latexparticles/LatexDripParticle.java
+++ src/main/java/net/ltxprogrammer/changed/client/latexparticles/LatexDripParticle.java
@@ -6,6 +6,7 @@ import com.mojang.math.Vector3f;
 import com.mojang.math.Vector4f;
 import net.ltxprogrammer.changed.client.ModelPartStem;
 import net.ltxprogrammer.changed.client.PoseStackExtender;
+import net.ltxprogrammer.changed.entity.LatexEntity;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.Camera;
 import net.minecraft.client.model.geom.ModelPart;
@@ -42,8 +43,9 @@ public class LatexDripParticle extends LatexParticle {
     protected int ticksAttached = 0;
     private final int maxTicksAttached;
 
-    private final LivingEntity attachedEntity;
-    private final ModelPartStem attachedPart;
+    private final LatexEntity attachedEntity;
+    private final ModelPart attachedPart;
+
     private final SurfacePoint surface;
     protected final Color3 color;
     protected final float alpha;
@@ -51,7 +53,7 @@ public class LatexDripParticle extends LatexParticle {
     private boolean prepped = false;
 
     public LatexDripParticle(SpriteSet spriteSet,
-                             LivingEntity attachedEntity, ModelPartStem attachedPart, SurfacePoint surface, Color3 color, float alpha, int lifespan) {
+                             LatexEntity attachedEntity, ModelPart attachedPart, SurfacePoint surface, Color3 color, float alpha, int lifespan) {
         super(attachedEntity.level, lifespan);
         this.maxTicksAttached = attachedEntity.level.random.nextInt(80, 2400);
 
@@ -173,6 +175,9 @@ public class LatexDripParticle extends LatexParticle {
                 .multiply(-1, -1, -1);*/
 
         poseStack.pushPose();
+        if (attachedEntity.getUnderlyingPlayer() != null && attachedEntity.isCrouching())
+            poseStack.translate(0.0, -0.125, 0.0); // This is to match the offset in the PlayerRenderer. TODO maybe mixin remove offset if player is latex?
+
         attachedPart.translateAndRotate(poseStack); // TODO capture parent ModelPart chain to get correct translation/rotation
         // in C = A * B, this is C
         var modelSpaceToScreenSpace = poseStack.last().pose();
@@ -293,7 +298,7 @@ public class LatexDripParticle extends LatexParticle {
         return LatexParticleRenderType.LATEX_PARTICLE_SHEET_3D_OPAQUE;
     }
 
-    public static LatexParticleProvider<LatexDripParticle> of(LivingEntity attachedEntity, ModelPartStem attachedPart, SurfacePoint surface, Color3 color, float alpha, int lifespan) {
+    public static LatexParticleProvider<LatexDripParticle> of(LatexEntity attachedEntity, ModelPart attachedPart, SurfacePoint surface, Color3 color, float alpha, int lifespan) {
         return new LatexParticleProvider<>() {
             @Override
             public LatexParticleType<LatexDripParticle> getParticleType() {
