34aa7de69ba1e311b39aca2e3645eb53e1f231e7
LtxPgm <ltxprogrammer@outlook.com>
2023-12-14 21:18:30 -0600

    Added custom coat layer that inherits the players hair color
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/AerosolLatexWolfRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/AerosolLatexWolfRenderer.java
@@ -1,6 +1,7 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomCoatLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.AerosolLatexWolfModel;
@@ -13,7 +14,7 @@ public class AerosolLatexWolfRenderer extends LatexHumanoidRenderer<AerosolLatex
     public AerosolLatexWolfRenderer(EntityRendererProvider.Context context) {
         super(context, new AerosolLatexWolfModel(context.bakeLayer( AerosolLatexWolfModel.LAYER_LOCATION)),
                 ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
-        this.addLayer(new LatexParticlesLayer<>(this, this.getModel()));
+        this.addLayer(new CustomCoatLayer<>(this, this.getModel(), Changed.modResource("textures/latex_gas_wolf_coat")));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer::always, CustomEyesLayer::always));
     }
 
