33ba4792f063ea00972f98c2914e87873f6b3a9f
LtxPgm <ltxprogrammer@outlook.com>
2023-12-14 18:12:46 -0600

    Added custom eye renderers to certain latex models
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedEntityRenderers.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedEntityRenderers.java
@@ -19,8 +19,7 @@ public class ChangedEntityRenderers {
         event.registerEntityRenderer(ChangedEntities.BEHEMOTH_HAND_LEFT.get(), BehemothHandLeftRenderer::new);
         event.registerEntityRenderer(ChangedEntities.BEHEMOTH_HAND_RIGHT.get(), BehemothHandRightRenderer::new);
 
-        event.registerEntityRenderer(ChangedEntities.AEROSOL_LATEX_WOLF.get(),
-                useNewModels ? AerosolLatexWolfRenderer.Remodel::new : AerosolLatexWolfRenderer::new);
+        event.registerEntityRenderer(ChangedEntities.AEROSOL_LATEX_WOLF.get(), AerosolLatexWolfRenderer::new);
         event.registerEntityRenderer(ChangedEntities.DARK_LATEX_DRAGON.get(),
                 useNewModels ? DarkLatexDragonRenderer.Remodel::new : DarkLatexDragonRenderer::new);
         event.registerEntityRenderer(ChangedEntities.DARK_LATEX_WOLF_FEMALE.get(), DarkLatexWolfFemaleRenderer::new);
