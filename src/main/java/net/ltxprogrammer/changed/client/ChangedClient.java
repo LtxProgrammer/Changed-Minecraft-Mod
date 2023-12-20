6d5ee7c4bcc31d3368931b99e38fb2c3263db2f0
LtxPgm <ltxprogrammer@outlook.com>
2023-12-20 15:11:41 -0600

    Adjusted latex particle generation to occur on client tick, instead of each render call
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/ChangedClient.java
+++ src/main/java/net/ltxprogrammer/changed/client/ChangedClient.java
@@ -2,7 +2,14 @@ package net.ltxprogrammer.changed.client;
 
 import net.ltxprogrammer.changed.client.latexparticles.LatexParticleEngine;
 import net.ltxprogrammer.changed.client.renderer.blockentity.ChangedBlockEntityWithoutLevelRenderer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
+import net.ltxprogrammer.changed.entity.LatexEntity;
+import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
@@ -27,7 +34,34 @@ public class ChangedClient {
             particleSystem.render(event.getPoseStack(), minecraft.gameRenderer.lightTexture(), event.getCamera(), event.getPartialTick(), event.getFrustum());
     }
 
+    protected static void addLatexParticleToEntity(LatexEntity entity) {
+        if (entity.getRandom().nextFloat() > entity.getDripRate(1.0f - entity.computeHealthRatio()))
+            return;
+        var renderer = minecraft.getEntityRenderDispatcher().getRenderer(entity);
+        if (!(renderer instanceof LivingEntityRenderer<?,?> livingEntityRenderer))
+            return;
+        for (var layer : livingEntityRenderer.layers) {
+            if (layer instanceof LatexParticlesLayer<?,?> latexParticlesLayer) {
+                latexParticlesLayer.createNewDripParticle(entity);
+                break;
+            }
+        }
+    }
+
+    protected static void addLatexParticleToEntity(Player entity) {
+        ProcessTransfur.ifPlayerLatex(entity, variant -> {
+            addLatexParticleToEntity(variant.getLatexEntity());
+        });
+    }
+
     public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (minecraft.level != null) {
+            var cameraPos = minecraft.gameRenderer.getMainCamera().getBlockPosition();
+            var aabb = AABB.of(BoundingBox.fromCorners(cameraPos.offset(-64, -64, -64), cameraPos.offset(64, 64, 64)));
+            minecraft.level.getEntitiesOfClass(LatexEntity.class, aabb).forEach(ChangedClient::addLatexParticleToEntity);
+            minecraft.level.getEntitiesOfClass(Player.class, aabb).forEach(ChangedClient::addLatexParticleToEntity);
+        }
+
         particleSystem.tick();
         clientTicks++;
     }
