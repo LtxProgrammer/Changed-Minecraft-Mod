06f6c161577e9aadf7014eaa83c490835ebcb3a5
LtxPgm <ltxprogrammer@outlook.com>
2023-12-21 11:42:40 -0600

    Implemented new armor models for the models
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LightLatexWolfFemaleRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LightLatexWolfFemaleRenderer.java
@@ -4,15 +4,16 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LightLatexWolfFemaleModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexFemaleWolfModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexWolfModel;
 import net.ltxprogrammer.changed.entity.beast.LightLatexWolfFemale;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LightLatexWolfFemaleRenderer extends LatexHumanoidRenderer<LightLatexWolfFemale, LightLatexWolfFemaleModel, ArmorLatexWolfModel<LightLatexWolfFemale>> {
+public class LightLatexWolfFemaleRenderer extends LatexHumanoidRenderer<LightLatexWolfFemale, LightLatexWolfFemaleModel, ArmorLatexFemaleWolfModel<LightLatexWolfFemale>> {
     public LightLatexWolfFemaleRenderer(EntityRendererProvider.Context context) {
         super(context, new LightLatexWolfFemaleModel(context.bakeLayer(LightLatexWolfFemaleModel.LAYER_LOCATION)),
-                ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexFemaleWolfModel::new, ArmorLatexFemaleWolfModel.INNER_ARMOR, ArmorLatexFemaleWolfModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer::always, CustomEyesLayer::always));
     }
