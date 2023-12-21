918ad171d70de412bc6be92eb4436481708c5a57
LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>
2023-12-20 21:41:04 -0600

    Bijimio Experimental Fork V5.4.4 | Implemented New Crystal Wolf & WL Drone Models. (#628)
    Co-authored-by: Bijimio

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexCrystalWolfHornedRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexCrystalWolfHornedRenderer.java
@@ -1,6 +1,8 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexCrystalWolfHornedModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexWolfModel;
 import net.ltxprogrammer.changed.entity.beast.LatexCrystalWolfHorned;
@@ -11,22 +13,11 @@ public class LatexCrystalWolfHornedRenderer extends LatexHumanoidRenderer<LatexC
     public LatexCrystalWolfHornedRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexCrystalWolfHornedModel(context.bakeLayer(LatexCrystalWolfHornedModel.LAYER_LOCATION)),
                 ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer::always, CustomEyesLayer::always));
     }
 
     @Override
     public ResourceLocation getTextureLocation(LatexCrystalWolfHorned p_114482_) {
         return Changed.modResource("textures/latex_crystal_wolf_horned.png");
-    }
-
-    public static class Remodel extends LatexHumanoidRenderer<LatexCrystalWolfHorned, LatexCrystalWolfHornedModel.Remodel, ArmorLatexWolfModel.RemodelMale<LatexCrystalWolfHorned>> {
-        public Remodel(EntityRendererProvider.Context context) {
-            super(context, new LatexCrystalWolfHornedModel.Remodel(context.bakeLayer(LatexCrystalWolfHornedModel.LAYER_LOCATION)),
-                    ArmorLatexWolfModel.RemodelMale::new, ArmorLatexWolfModel.RemodelMale.INNER_ARMOR, ArmorLatexWolfModel.RemodelMale.OUTER_ARMOR, 0.5f);
-        }
-
-        @Override
-        public ResourceLocation getTextureLocation(LatexCrystalWolfHorned p_114482_) {
-            return Changed.modResource("textures/remodel/latex_crystal_wolf_horned.png");
-        }
     }
 }
