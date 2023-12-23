e43897a686a5e5948bafd5fe9a86c852f172a14d
bijimio <117211742+bijimio@users.noreply.github.com>
2023-12-23 09:42:57 -0800

    Modeled and Implemented Unified Latex Keon, Attempted to Implement Potted Orange Tree Sapling. (#637)
    * Bijimio Experimental Fork V5.4.9 | Modeled and Implemented Latex Keon
    
    * Bijimio Experimental Fork V5.4.9.5 | Fixed error
    
    * Bijimio Experimental Fork V5.5.0 | Attempted Implementation Of Potted range tree Sapling.
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSilverFoxRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSilverFoxRenderer.java
@@ -1,20 +1,24 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexSilverFoxModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexWolfModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
 import net.ltxprogrammer.changed.entity.beast.LatexSilverFox;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexSilverFoxRenderer extends LatexHumanoidRenderer<LatexSilverFox, LatexSilverFoxModel, ArmorLatexWolfModel<LatexSilverFox>> {
+public class LatexSilverFoxRenderer extends LatexHumanoidRenderer<LatexSilverFox, LatexSilverFoxModel, ArmorLatexMaleWolfModel<LatexSilverFox>> {
     public LatexSilverFoxRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexSilverFoxModel(context.bakeLayer(LatexSilverFoxModel.LAYER_LOCATION)),
-                ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleWolfModel::new, ArmorLatexMaleWolfModel.INNER_ARMOR, ArmorLatexMaleWolfModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
     }
 
     @Override
     public ResourceLocation getTextureLocation(LatexSilverFox p_114482_) {
-        return Changed.modResource("textures/latex_silver_fox.png");
+        return Changed.modResource("textures/latex_keon.png");
     }
 }
