68ad56be8a6c18ff4fb7b12591600ca8446bfb11
bijimio <117211742+bijimio@users.noreply.github.com>
2023-12-28 17:07:00 -0800

    Adjusted & Implemented Beifeng Model, Created Set Color For Purple Fox Eyes. (#650)
    * Bijimio Experimental Fork V5.5.5 | Adjusted & Implemented Beifeng Model, Created Set Color For Purple Fox Eyes
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedLayerDefinitions.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedLayerDefinitions.java
@@ -59,8 +59,7 @@ public class ChangedLayerDefinitions {
         ForgeHooksClient.registerLayerDefinition(HeadlessKnightModel.LAYER_LOCATION, HeadlessKnightModel::createBodyLayer);
         ForgeHooksClient.registerLayerDefinition(LatexAlienModel.LAYER_LOCATION, LatexAlienModel::createBodyLayer);
         ForgeHooksClient.registerLayerDefinition(LatexBeeModel.LAYER_LOCATION, LatexBeeModel::createBodyLayer);
-        ForgeHooksClient.registerLayerDefinition(LatexBeifengModel.LAYER_LOCATION,
-                useNewModels ? LatexBeifengModel.Remodel::createBodyLayer : LatexBeifengModel::createBodyLayer);
+        ForgeHooksClient.registerLayerDefinition(LatexBeifengModel.LAYER_LOCATION, LatexBeifengModel::createBodyLayer);
         ForgeHooksClient.registerLayerDefinition(LatexBenignWolfModel.LAYER_LOCATION, LatexBenignWolfModel::createBodyLayer);
         ForgeHooksClient.registerLayerDefinition(LatexBlueDragonModel.LAYER_LOCATION, LatexBlueDragonModel::createBodyLayer);
         ForgeHooksClient.registerLayerDefinition(LatexBlueWolfModel.LAYER_LOCATION, LatexBlueWolfModel::createBodyLayer);
