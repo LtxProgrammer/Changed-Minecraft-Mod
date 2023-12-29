2620ccad11a184050ce8c8411a6268e7d1890f80
LtxPgm <ltxprogrammer@outlook.com>
2023-12-28 22:28:32 -0600

    Adjusted dragon-like animator

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/upperbody/DragonUpperBodyCrouchAnimator.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/upperbody/DragonUpperBodyCrouchAnimator.java
@@ -19,14 +19,14 @@ public class DragonUpperBodyCrouchAnimator<T extends LatexEntity, M extends Enti
 
     @Override
     public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        torso.z = Mth.lerp(core.ageLerp, -1.0f, -1.5f);
+        torso.z = -1.0f;//Mth.lerp(core.ageLerp, -1.0f, -1.5f);
         head.z = torso.z;
 
-        torso.xRot = Mth.lerp(core.ageLerp, 0.5f, 0.6f);
+        torso.xRot = 0.5f;//Mth.lerp(core.ageLerp, 0.5f, 0.6f);
         rightArm.xRot += 0.3F;
         leftArm.xRot += 0.3F;
 
-        torso.y = Mth.lerp(core.ageLerp, 3.2f, 4.0f) + core.hipOffset + (12.0f - core.legLength);
+        torso.y = 3.2f/*Mth.lerp(core.ageLerp, 3.2f, 4.0f)*/ + core.hipOffset + (12.0f - core.legLength);
         head.y = torso.y + 0.5f;
         leftArm.y = torso.y + 2.25f;
         rightArm.y = torso.y + 2.25f;
