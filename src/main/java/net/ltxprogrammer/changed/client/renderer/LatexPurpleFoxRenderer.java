d3db81b771732e7627af296f09dd671430ed2d9d
Bijimio <mp39@tutanota.com>
2023-12-29 02:07:12 -0800

    Bijimio Experimental Fork V5.5.5 | Adjusted & Implemented Beifeng Model, Created Set Color For Purple Fox Eyes

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexPurpleFoxRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexPurpleFoxRenderer.java
@@ -6,6 +6,7 @@ import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexPurpleFoxModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
 import net.ltxprogrammer.changed.entity.beast.LatexPurpleFox;
+import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -14,7 +15,7 @@ public class LatexPurpleFoxRenderer extends LatexHumanoidRenderer<LatexPurpleFox
         super(context, new LatexPurpleFoxModel(context.bakeLayer(LatexPurpleFoxModel.LAYER_LOCATION)),
                 ArmorLatexMaleWolfModel::new, ArmorLatexMaleWolfModel.INNER_ARMOR, ArmorLatexMaleWolfModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
-        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.WHITE),CustomEyesLayer.fixedColor(Color3.parseHex("#43b44e"))));
     }
 
     @Override
