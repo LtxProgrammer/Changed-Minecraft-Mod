e375540fb979f4e40aad6ca22d94d48de50f434a
LtxPgm <ltxprogrammer@outlook.com>
2023-12-30 11:02:25 -0600

    Added idle wing animation

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/wing/DragonWingCreativeFlyAnimator.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/wing/DragonWingCreativeFlyAnimator.java
@@ -7,10 +7,6 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Animator that handles a slithering entity upright on land
- * @param <T>
- */
 public class DragonWingCreativeFlyAnimator<T extends LatexEntity, M extends EntityModel<T>> extends AbstractWingAnimatorV2<T, M> {
     public DragonWingCreativeFlyAnimator(
             ModelPart leftWingRoot, ModelPart leftWingBone1, ModelPart leftWingBone2,
@@ -25,17 +21,6 @@ public class DragonWingCreativeFlyAnimator<T extends LatexEntity, M extends Enti
 
     @Override
     public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        leftWingRoot.xRot = Mth.lerp(core.fallFlyingAmount, leftWingRoot.xRot, 0.0f);
-        leftWingRoot.yRot = Mth.lerp(core.fallFlyingAmount, leftWingRoot.yRot, Mth.DEG_TO_RAD * 5.0f);
-        leftWingRoot.zRot = Mth.lerp(core.fallFlyingAmount, leftWingRoot.zRot, 0.0f);
-        rightWingRoot.xRot = Mth.lerp(core.fallFlyingAmount, rightWingRoot.xRot, 0.0f);
-        rightWingRoot.yRot = Mth.lerp(core.fallFlyingAmount, rightWingRoot.yRot, Mth.DEG_TO_RAD * -5.0f);
-        rightWingRoot.zRot = Mth.lerp(core.fallFlyingAmount, rightWingRoot.zRot, 0.0f);
 
-        leftWingBone1.zRot = Mth.lerp(core.fallFlyingAmount, leftWingBone1.zRot, Mth.DEG_TO_RAD * 30.0f);
-        leftWingBone2.zRot = Mth.lerp(core.fallFlyingAmount, leftWingBone2.zRot, Mth.DEG_TO_RAD * 55.0f);
-
-        rightWingBone1.zRot = Mth.lerp(core.fallFlyingAmount, rightWingBone1.zRot, Mth.DEG_TO_RAD * 30.0f);
-        rightWingBone2.zRot = Mth.lerp(core.fallFlyingAmount, rightWingBone2.zRot, Mth.DEG_TO_RAD * 55.0f);
     }
 }
