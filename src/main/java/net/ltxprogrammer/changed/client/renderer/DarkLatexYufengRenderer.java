92a4bdc354471be882748307c7e972b17a6e7a21
bijimio <117211742+bijimio@users.noreply.github.com>
2023-12-30 08:01:56 -0800

    Implemented DL Yufeng (Needs Animator) Fixed Texture Errors with Crystal Wolves. (#656)
    * Bijimio Experimental Fork V5.5.9 | Implemented DL Yufeng.

--- src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexYufengRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexYufengRenderer.java
@@ -1,9 +1,12 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.DarkLatexYufengModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBlueDragonModel;
 import net.ltxprogrammer.changed.entity.beast.DarkLatexYufeng;
+import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -11,22 +14,13 @@ public class DarkLatexYufengRenderer extends LatexHumanoidRenderer<DarkLatexYufe
     public DarkLatexYufengRenderer(EntityRendererProvider.Context context) {
         super(context, new DarkLatexYufengModel(context.bakeLayer(DarkLatexYufengModel.LAYER_LOCATION)),
                 ArmorLatexBlueDragonModel::new, ArmorLatexBlueDragonModel.INNER_ARMOR, ArmorLatexBlueDragonModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.parseHex("#303030")),
+                CustomEyesLayer.fixedIfNotDarkLatexOverride(Color3.WHITE)));
     }
 
     @Override
     public ResourceLocation getTextureLocation(DarkLatexYufeng p_114482_) {
         return Changed.modResource("textures/dark_latex_yufeng.png");
-    }
-
-    public static class Remodel extends LatexHumanoidRenderer<DarkLatexYufeng, DarkLatexYufengModel.Remodel, ArmorLatexBlueDragonModel.RemodelMale<DarkLatexYufeng>> {
-        public Remodel(EntityRendererProvider.Context context) {
-            super(context, new DarkLatexYufengModel.Remodel(context.bakeLayer(DarkLatexYufengModel.LAYER_LOCATION)),
-                    ArmorLatexBlueDragonModel.RemodelMale::new, ArmorLatexBlueDragonModel.RemodelMale.INNER_ARMOR, ArmorLatexBlueDragonModel.RemodelMale.OUTER_ARMOR, 0.5f);
-        }
-
-        @Override
-        public ResourceLocation getTextureLocation(DarkLatexYufeng p_114482_) {
-            return Changed.modResource("textures/remodel/dark_latex_yufeng.png");
-        }
     }
 }
