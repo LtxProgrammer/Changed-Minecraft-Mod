6b034a7b545d4ffdc0a9f5305b0a7ec3254ca690
bijimio <117211742+bijimio@users.noreply.github.com>
2024-01-01 08:18:32 -0800

    Implemented Latex Translucent Lizard Model. (#660)
    * Bijimio Experimental Fork V5.6.4 | Implemented Translucent Lizard
    
    ---------
    
    Co-authored-by: LtxProgrammer <106349281+LtxProgrammer@users.noreply.github.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexTranslucentLizardRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexTranslucentLizardRenderer.java
@@ -1,10 +1,13 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexTranslucentLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexTranslucentLizardModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBlueDragonModel;
 import net.ltxprogrammer.changed.entity.beast.LatexTranslucentLizard;
+import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -12,24 +15,13 @@ public class LatexTranslucentLizardRenderer extends LatexHumanoidRenderer<LatexT
     public LatexTranslucentLizardRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexTranslucentLizardModel(context.bakeLayer(LatexTranslucentLizardModel.LAYER_LOCATION)),
                 ArmorLatexBlueDragonModel::new, ArmorLatexBlueDragonModel.INNER_ARMOR, ArmorLatexBlueDragonModel.OUTER_ARMOR, 0.5f);
-        this.addLayer(new LatexTranslucentLayer<>(this, this.model, Changed.modResource("textures/latex_translucent_lizard_translucent.png")));
+        this.addLayer(new LatexTranslucentLayer<>(this, this.model, Changed.modResource("textures/latex_translucent_lizard_outer.png")));
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.parseHex("#ffb84c")),CustomEyesLayer.fixedColor(Color3.parseHex("#a24b42"))));
     }
 
     @Override
     public ResourceLocation getTextureLocation(LatexTranslucentLizard p_114482_) {
-        return Changed.modResource("textures/latex_translucent_lizard.png");
-    }
-
-    public static class Remodel extends LatexHumanoidRenderer<LatexTranslucentLizard, LatexTranslucentLizardModel.Remodel, ArmorLatexBlueDragonModel.RemodelMale<LatexTranslucentLizard>> {
-        public Remodel(EntityRendererProvider.Context context) {
-            super(context, new LatexTranslucentLizardModel.Remodel(context.bakeLayer(LatexTranslucentLizardModel.LAYER_LOCATION)),
-                    ArmorLatexBlueDragonModel.RemodelMale::new, ArmorLatexBlueDragonModel.RemodelMale.INNER_ARMOR, ArmorLatexBlueDragonModel.RemodelMale.OUTER_ARMOR, 0.5f);
-            //this.addLayer(new LatexTranslucentLayer<>(this, new LatexTranslucentLizardModel.Remodel(context.bakeLayer(LatexTranslucentLizardModel.LAYER_LOCATION_OUTER))));
-        }
-
-        @Override
-        public ResourceLocation getTextureLocation(LatexTranslucentLizard p_114482_) {
-            return Changed.modResource("textures/remodel/latex_translucent_lizard.png");
-        }
+        return Changed.modResource("textures/latex_translucent_lizard_inner.png");
     }
 }
