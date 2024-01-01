ac9b524b8cb2273bc3901f6e0324342dee806095
LtxPgm <ltxprogrammer@outlook.com>
2024-01-01 10:55:37 -0600

    Created armor layers for dragons and winged dragons

--- src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexYufengRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexYufengRenderer.java
@@ -4,16 +4,16 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.DarkLatexYufengModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBlueDragonModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWingedDragonModel;
 import net.ltxprogrammer.changed.entity.beast.DarkLatexYufeng;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class DarkLatexYufengRenderer extends LatexHumanoidRenderer<DarkLatexYufeng, DarkLatexYufengModel, ArmorLatexBlueDragonModel<DarkLatexYufeng>> {
+public class DarkLatexYufengRenderer extends LatexHumanoidRenderer<DarkLatexYufeng, DarkLatexYufengModel, ArmorLatexMaleWingedDragonModel<DarkLatexYufeng>> {
     public DarkLatexYufengRenderer(EntityRendererProvider.Context context) {
         super(context, new DarkLatexYufengModel(context.bakeLayer(DarkLatexYufengModel.LAYER_LOCATION)),
-                ArmorLatexBlueDragonModel::new, ArmorLatexBlueDragonModel.INNER_ARMOR, ArmorLatexBlueDragonModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleWingedDragonModel::new, ArmorLatexMaleWingedDragonModel.INNER_ARMOR, ArmorLatexMaleWingedDragonModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.parseHex("#303030")),
                 CustomEyesLayer.fixedIfNotDarkLatexOverride(Color3.WHITE)));
--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexPinkYuinDragonRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexPinkYuinDragonRenderer.java
@@ -4,16 +4,16 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexPinkYuinDragonModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBlueDragonModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWingedDragonModel;
 import net.ltxprogrammer.changed.entity.beast.LatexPinkYuinDragon;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexPinkYuinDragonRenderer extends LatexHumanoidRenderer<LatexPinkYuinDragon, LatexPinkYuinDragonModel, ArmorLatexBlueDragonModel<LatexPinkYuinDragon>>  {
+public class LatexPinkYuinDragonRenderer extends LatexHumanoidRenderer<LatexPinkYuinDragon, LatexPinkYuinDragonModel, ArmorLatexMaleWingedDragonModel<LatexPinkYuinDragon>>  {
     public LatexPinkYuinDragonRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexPinkYuinDragonModel(context.bakeLayer(LatexPinkYuinDragonModel.LAYER_LOCATION)),
-                ArmorLatexBlueDragonModel::new, ArmorLatexBlueDragonModel.INNER_ARMOR, ArmorLatexBlueDragonModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleWingedDragonModel::new, ArmorLatexMaleWingedDragonModel.INNER_ARMOR, ArmorLatexMaleWingedDragonModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.WHITE),CustomEyesLayer.fixedColor(Color3.parseHex("#7889f3"))));
     }
