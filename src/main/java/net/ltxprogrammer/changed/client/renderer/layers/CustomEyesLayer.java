0c0d5781da3c1820a98137825e7d5fcab0d7310e
LtxPgm <ltxprogrammer@outlook.com>
2024-01-01 11:28:16 -0600

    Added translucency and emissive configuration to CustomEyesLayer

--- src/main/java/net/ltxprogrammer/changed/client/renderer/layers/CustomEyesLayer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/layers/CustomEyesLayer.java
@@ -26,17 +26,44 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CustomEyesLayer<M extends LatexHumanoidModel<T>, T extends LatexEntity> extends RenderLayer<T, M> {
-    public interface ColorFunction<T extends LatexEntity> extends BiFunction<T, BasicPlayerInfo, Color3> {
+    public static class ColorData {
+        public final Color3 color;
+        public final float alpha;
+        public final boolean emissive;
+
+        private ColorData(Color3 color, float alpha, boolean emissive) {
+            this.color = color;
+            this.alpha = alpha;
+            this.emissive = emissive;
+        }
+
+        public static ColorData ofColor(Color3 color) {
+            return new ColorData(color, 1.0f, false);
+        }
+
+        public static ColorData ofTranslucentColor(Color3 color, float alpha) {
+            return new ColorData(color, alpha, false);
+        }
+
+        public static ColorData ofEmissiveColor(Color3 color) {
+            return new ColorData(color, 1.0f, true);
+        }
+    }
+
+    public interface ColorFunction<T extends LatexEntity> extends BiFunction<T, BasicPlayerInfo, ColorData> {
         @Nullable
-        Color3 getColor(T entity, BasicPlayerInfo bpi);
+        ColorData getColor(T entity, BasicPlayerInfo bpi);
 
         @Override
-        default Color3 apply(T entity, BasicPlayerInfo bpi) {
+        default ColorData apply(T entity, BasicPlayerInfo bpi) {
             return getColor(entity, bpi);
         }
 
-        default Optional<Color3> getColorSafe(T entity, BasicPlayerInfo bpi) {
-            return Optional.ofNullable(this.getColor(entity, bpi));
+        default Optional<ColorData> getColorSafe(T entity, BasicPlayerInfo bpi) {
+            var safe = Optional.ofNullable(this.getColor(entity, bpi));
+            if (safe.isPresent() && safe.get().alpha <= 0.0f)
+                return Optional.empty();
+            return safe;
         }
     }
 
@@ -46,28 +73,44 @@ public class CustomEyesLayer<M extends LatexHumanoidModel<T>, T extends LatexEnt
     private final ColorFunction<T> irisColorFn;
     private final ColorFunction<T> eyeBrowsColorFn;
 
-    public static <T extends LatexEntity> Color3 noRender(T entity, BasicPlayerInfo bpi) {
+    public static <T extends LatexEntity> ColorData noRender(T entity, BasicPlayerInfo bpi) {
         return null;
     }
 
-    public static <T extends LatexEntity> Color3 irisColor(T entity, BasicPlayerInfo bpi) {
-        return bpi.getIrisColor();
+    public static <T extends LatexEntity> ColorData irisColor(T entity, BasicPlayerInfo bpi) {
+        return ColorData.ofColor(bpi.getIrisColor());
     }
 
-    public static <T extends LatexEntity> Color3 scleraColor(T entity, BasicPlayerInfo bpi) {
-        return bpi.getScleraColor();
+    public static <T extends LatexEntity> ColorData glowingIrisColor(T entity, BasicPlayerInfo bpi) {
+        return ColorData.ofEmissiveColor(bpi.getIrisColor());
     }
 
-    public static <T extends LatexEntity> Color3 hairColor(T entity, BasicPlayerInfo bpi) {
-        return bpi.getHairColor();
+    public static <T extends LatexEntity> ColorFunction<T> translucentIrisColor(float alpha) {
+        return (entity, bpi) -> ColorData.ofTranslucentColor(bpi.getIrisColor(), alpha);
+    }
+
+    public static <T extends LatexEntity> ColorData scleraColor(T entity, BasicPlayerInfo bpi) {
+        return ColorData.ofColor(bpi.getScleraColor());
+    }
+
+    public static <T extends LatexEntity> ColorData hairColor(T entity, BasicPlayerInfo bpi) {
+        return ColorData.ofColor(bpi.getHairColor());
     }
 
     public static <T extends LatexEntity> ColorFunction<T> fixedColor(Color3 color) {
-        return (entity, bpi) -> color;
+        return (entity, bpi) -> ColorData.ofColor(color);
+    }
+
+    public static <T extends LatexEntity> ColorFunction<T> fixedColorGlowing(Color3 color) {
+        return (entity, bpi) -> ColorData.ofEmissiveColor(color);
+    }
+
+    public static <T extends LatexEntity> ColorFunction<T> fixedColor(Color3 color, float alpha) {
+        return (entity, bpi) -> ColorData.ofTranslucentColor(color, alpha);
     }
 
     public static <T extends LatexEntity> ColorFunction<T> fixedIfNotDarkLatexOverride(Color3 color) {
-        return (entity, bpi) -> bpi.isOverrideIrisOnDarkLatex() ? bpi.getIrisColor() : color;
+        return (entity, bpi) -> ColorData.ofColor(bpi.isOverrideIrisOnDarkLatex() ? bpi.getIrisColor() : color);
     }
 
     public CustomEyesLayer(RenderLayerParent<T, M> parent, EntityModelSet modelSet) {
@@ -100,6 +143,10 @@ public class CustomEyesLayer<M extends LatexHumanoidModel<T>, T extends LatexEnt
         head.render(pose, buffer, packedLight, overlay, color.red(), color.green(), color.blue(), alpha);
     }
 
+    private RenderType renderTypeForData(ColorData data, ResourceLocation texture) {
+        return data.alpha < 1.0f ? RenderType.entityTranslucent(texture) : (data.emissive ? RenderType.eyes(texture) : RenderType.entityCutout(texture));
+    }
+
     @Override
     public void render(PoseStack pose, MultiBufferSource bufferSource, int packedLight, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         var info = entity.getBasicPlayerInfo();
@@ -109,14 +156,14 @@ public class CustomEyesLayer<M extends LatexHumanoidModel<T>, T extends LatexEnt
 
         head.copyFrom(this.getParentModel().getHead());
 
-        scleraColorFn.getColorSafe(entity, info).ifPresent(color -> {
-            renderHead(pose, bufferSource.getBuffer(RenderType.entityCutout(style.getSclera())), packedLight, overlay, color, 1.0F);
+        scleraColorFn.getColorSafe(entity, info).ifPresent(data -> {
+            renderHead(pose, bufferSource.getBuffer(renderTypeForData(data, style.getSclera())), packedLight, overlay, data.color, data.alpha);
         });
-        irisColorFn.getColorSafe(entity, info).ifPresent(color -> {
-            renderHead(pose, bufferSource.getBuffer(RenderType.entityCutout(style.getIris())), packedLight, overlay, color, 1.0F);
+        irisColorFn.getColorSafe(entity, info).ifPresent(data -> {
+            renderHead(pose, bufferSource.getBuffer(renderTypeForData(data, style.getIris())), packedLight, overlay, data.color, data.alpha);
         });
-        eyeBrowsColorFn.getColorSafe(entity, info).ifPresent(color -> {
-            renderHead(pose, bufferSource.getBuffer(RenderType.entityCutout(style.getEyeBrows())), packedLight, overlay, color, 1.0F);
+        eyeBrowsColorFn.getColorSafe(entity, info).ifPresent(data -> {
+            renderHead(pose, bufferSource.getBuffer(renderTypeForData(data, style.getEyeBrows())), packedLight, overlay, data.color, data.alpha);
         });
     }
 }
