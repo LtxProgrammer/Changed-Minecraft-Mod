d13648f0e5c3a2bad3bade964f839f9351e2e904
bijimio <117211742+bijimio@users.noreply.github.com>
2024-01-02 20:07:52 -0800

    Modeled & Implemented Unified Red Dragon, Adjusted Winged Dragon Armor Models. (#662)
    * Bijimio Experimental Fork V5.6.5 | Modeled & Implemented Unified Red Dragon, Adjusted Winged Dragon Armor Models

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexRedDragonRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexRedDragonRenderer.java
@@ -1,16 +1,21 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexRedDragonModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBlueDragonModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWingedDragonModel;
 import net.ltxprogrammer.changed.entity.beast.LatexRedDragon;
+import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexRedDragonRenderer extends LatexHumanoidRenderer<LatexRedDragon, LatexRedDragonModel, ArmorLatexBlueDragonModel<LatexRedDragon>>  {
+public class LatexRedDragonRenderer extends LatexHumanoidRenderer<LatexRedDragon, LatexRedDragonModel, ArmorLatexMaleWingedDragonModel<LatexRedDragon>>  {
            public LatexRedDragonRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexRedDragonModel(context.bakeLayer(LatexRedDragonModel.LAYER_LOCATION)),
-                ArmorLatexBlueDragonModel::new, ArmorLatexBlueDragonModel.INNER_ARMOR, ArmorLatexBlueDragonModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleWingedDragonModel::new, ArmorLatexMaleWingedDragonModel.INNER_ARMOR, ArmorLatexMaleWingedDragonModel.OUTER_ARMOR, 0.5f);
+               this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+               this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.WHITE),CustomEyesLayer.fixedColor(Color3.parseHex("#ffe93f"))));
     }
 
     @Override
