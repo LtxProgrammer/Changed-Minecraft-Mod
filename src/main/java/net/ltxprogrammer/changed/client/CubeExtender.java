28204f95a3d5bd34f3dbac2c9ac60e00f2de5db3
LtxPgm <ltxprogrammer@outlook.com>
2024-01-04 12:52:54 -0600

    Added latex texture sample for latex drip particle
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/CubeExtender.java
+++ src/main/java/net/ltxprogrammer/changed/client/CubeExtender.java
@@ -1,8 +1,10 @@
 package net.ltxprogrammer.changed.client;
 
 import com.mojang.math.Vector3f;
+import net.minecraft.client.model.geom.builders.UVPair;
 
 public interface CubeExtender {
     Vector3f getVisualMin();
     Vector3f getVisualMax();
+    UVPair getUV(Vector3f surfacePoint);
 }
