dbf273f6751c740361d568fcb3772aa0a4f74e7f
LtxPgm <ltxprogrammer@outlook.com>
2024-01-04 21:04:59 -0600

    Added BPI screen to translations, added checkbox configs to BPI screen
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/gui/BasicPlayerInfoScreen.java
+++ src/main/java/net/ltxprogrammer/changed/client/gui/BasicPlayerInfoScreen.java
@@ -5,15 +5,17 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.entity.EyeStyle;
 import net.ltxprogrammer.changed.init.ChangedRegistry;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Checkbox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 
 public class BasicPlayerInfoScreen extends Screen {
     private final Screen lastScreen;
 
     public BasicPlayerInfoScreen(Screen parent) {
-        super(new TextComponent("Basic Player Info"));
+        super(new TranslatableComponent("changed.config.bpi.screen"));
         this.lastScreen = parent;
     }
 
@@ -29,29 +31,46 @@ public class BasicPlayerInfoScreen extends Screen {
     @Override
     protected void init() {
         super.init();
+        var bpi = Changed.config.client.basicPlayerInfo;
         int i = 0;
 
-        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TextComponent("Hair Color"),
-                Changed.config.client.basicPlayerInfo::getHairColor, Changed.config.client.basicPlayerInfo::setHairColor));
+        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.hair_color"),
+                bpi::getHairColor, bpi::setHairColor));
         i++;
-        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TextComponent("Iris Color"),
-                Changed.config.client.basicPlayerInfo::getIrisColor, Changed.config.client.basicPlayerInfo::setIrisColor));
+        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.iris_color"),
+                bpi::getIrisColor, bpi::setIrisColor));
         i++;
-        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TextComponent("Sclera Color"),
-                Changed.config.client.basicPlayerInfo::getScleraColor, Changed.config.client.basicPlayerInfo::setScleraColor));
+        this.addRenderableWidget(new ColorSelector(this.font, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.sclera_color"),
+                bpi::getScleraColor, bpi::setScleraColor));
         i++;
-        this.addRenderableWidget(new Button(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, Changed.config.client.basicPlayerInfo.getEyeStyle().getName(), button -> {
-            var style = Changed.config.client.basicPlayerInfo.getEyeStyle();
+        this.addRenderableWidget(new Button(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.eye_style", bpi.getEyeStyle().getName()), button -> {
+            var style = bpi.getEyeStyle();
             int id = style.ordinal();
             if (id < EyeStyle.values().length - 1)
                 id += 1;
             else
                 id = 0;
             style = EyeStyle.values()[id];
-            Changed.config.client.basicPlayerInfo.setEyeStyle(style);
-            button.setMessage(style.getName());
+            bpi.setEyeStyle(style);
+            button.setMessage(new TranslatableComponent("changed.config.bpi.eye_style", style.getName()));
         }));
         i++;
+        this.addRenderableWidget(new Checkbox(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.override_dl_iris"), bpi.isOverrideIrisOnDarkLatex()) {
+            @Override
+            public void onPress() {
+                super.onPress();
+                bpi.setOverrideIrisOnDarkLatex(this.selected());
+            }
+        });
+        i += 2;
+        this.addRenderableWidget(new Checkbox(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, new TranslatableComponent("changed.config.bpi.override_all_eye_styles"), bpi.isOverrideOthersToMatchStyle()) {
+            @Override
+            public void onPress() {
+                super.onPress();
+                bpi.setOverrideOthersToMatchStyle(this.selected());
+            }
+        });
+        i += 2;
 
         this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 6 + 24 * (i >> 1), 200, 20, CommonComponents.GUI_DONE, (p_96700_) -> {
             this.minecraft.setScreen(this.lastScreen);
