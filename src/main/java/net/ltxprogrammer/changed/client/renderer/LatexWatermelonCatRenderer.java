f754a156acb3ff0ae6f91dc898c40758feaac443
bijimio <117211742+bijimio@users.noreply.github.com>
2024-01-05 19:40:19 -0800

    Created Unified Cat Armors. (#678)
    * Bijimio Experimental Fork V5.7.4 | Created Cat Armor Models

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexWatermelonCatRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexWatermelonCatRenderer.java
@@ -4,16 +4,16 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexWatermelonCatModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexWolfModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexFemaleCatModel;
 import net.ltxprogrammer.changed.entity.beast.LatexWatermelonCat;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexWatermelonCatRenderer extends LatexHumanoidRenderer<LatexWatermelonCat, LatexWatermelonCatModel, ArmorLatexWolfModel<LatexWatermelonCat>> {
+public class LatexWatermelonCatRenderer extends LatexHumanoidRenderer<LatexWatermelonCat, LatexWatermelonCatModel, ArmorLatexFemaleCatModel<LatexWatermelonCat>> {
     public LatexWatermelonCatRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexWatermelonCatModel(context.bakeLayer(LatexWatermelonCatModel.LAYER_LOCATION)),
-                ArmorLatexWolfModel::new, ArmorLatexWolfModel.INNER_ARMOR, ArmorLatexWolfModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexFemaleCatModel::new, ArmorLatexFemaleCatModel.INNER_ARMOR, ArmorLatexFemaleCatModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, this.model));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(),
                 CustomEyesLayer.fixedColor(Color3.BLACK),
