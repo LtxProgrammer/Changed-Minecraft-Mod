095a3cbe217c3e54301f9b544716ef48cd3149f8
Bijimio <mp39@tutanota.com>
2024-01-06 04:34:43 -0800

    Bijimio Experimental Fork V5.7.4 | Created Cat Armor Models

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSnowLeopardFemaleRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSnowLeopardFemaleRenderer.java
@@ -4,15 +4,15 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexSnowLeopardFemaleModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexSnowLeopardModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexFemaleCatModel;
 import net.ltxprogrammer.changed.entity.beast.LatexSnowLeopardFemale;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexSnowLeopardFemaleRenderer extends LatexHumanoidRenderer<LatexSnowLeopardFemale, LatexSnowLeopardFemaleModel, ArmorLatexSnowLeopardModel<LatexSnowLeopardFemale>> {
+public class LatexSnowLeopardFemaleRenderer extends LatexHumanoidRenderer<LatexSnowLeopardFemale, LatexSnowLeopardFemaleModel, ArmorLatexFemaleCatModel<LatexSnowLeopardFemale>> {
     public LatexSnowLeopardFemaleRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexSnowLeopardFemaleModel(context.bakeLayer(LatexSnowLeopardFemaleModel.LAYER_LOCATION)),
-                ArmorLatexSnowLeopardModel::new, ArmorLatexSnowLeopardModel.INNER_ARMOR, ArmorLatexSnowLeopardModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexFemaleCatModel::new, ArmorLatexFemaleCatModel.INNER_ARMOR, ArmorLatexFemaleCatModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
     }
