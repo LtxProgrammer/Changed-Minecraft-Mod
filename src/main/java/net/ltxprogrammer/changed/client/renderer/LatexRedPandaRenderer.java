b4ae820b29bcd33fd5bac20159beea165298b254
Bijimio <mp39@tutanota.com>
2024-01-12 23:32:25 -0800

    Bijimio Experimental Fork V5.8.8 |

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexRedPandaRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexRedPandaRenderer.java
@@ -1,32 +1,24 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexRedPandaModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexSnowLeopardModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleCatModel;
 import net.ltxprogrammer.changed.entity.beast.LatexRedPanda;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexRedPandaRenderer extends LatexHumanoidRenderer<LatexRedPanda, LatexRedPandaModel, ArmorLatexSnowLeopardModel<LatexRedPanda>> {
+public class LatexRedPandaRenderer extends LatexHumanoidRenderer<LatexRedPanda, LatexRedPandaModel, ArmorLatexMaleCatModel<LatexRedPanda>> {
     public LatexRedPandaRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexRedPandaModel(context.bakeLayer(LatexRedPandaModel.LAYER_LOCATION)),
-                ArmorLatexSnowLeopardModel::new, ArmorLatexSnowLeopardModel.INNER_ARMOR, ArmorLatexSnowLeopardModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleCatModel::new, ArmorLatexMaleCatModel.INNER_ARMOR, ArmorLatexMaleCatModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
     }
 
     @Override
     public ResourceLocation getTextureLocation(LatexRedPanda p_114482_) {
         return Changed.modResource("textures/latex_red_panda.png");
-    }
-
-    public static class Remodel extends LatexHumanoidRenderer<LatexRedPanda, LatexRedPandaModel.Remodel, ArmorLatexSnowLeopardModel.RemodelMale<LatexRedPanda>> {
-        public Remodel(EntityRendererProvider.Context context) {
-            super(context, new LatexRedPandaModel.Remodel(context.bakeLayer(LatexRedPandaModel.LAYER_LOCATION)),
-                    ArmorLatexSnowLeopardModel.RemodelMale::new, ArmorLatexSnowLeopardModel.RemodelMale.INNER_ARMOR, ArmorLatexSnowLeopardModel.RemodelMale.OUTER_ARMOR, 0.5f);
-        }
-
-        @Override
-        public ResourceLocation getTextureLocation(LatexRedPanda p_114482_) {
-            return Changed.modResource("textures/remodel/latex_red_panda.png");
-        }
     }
 }
