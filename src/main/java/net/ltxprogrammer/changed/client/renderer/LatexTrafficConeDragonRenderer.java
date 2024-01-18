6ec9f7c1586063ea29fb8a578cbad7cc5986f391
Bijimio <mp39@tutanota.com>
2024-01-17 19:48:27 -0800

    Bijimio Experimental Fork V5.9.1 | Created Armor For Big Tail Latex Dragons

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexTrafficConeDragonRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexTrafficConeDragonRenderer.java
@@ -4,16 +4,16 @@ import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexTrafficConeDragonModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexTrafficConeDragonModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexBigTailDragonModel;
 import net.ltxprogrammer.changed.entity.beast.LatexTrafficConeDragon;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexTrafficConeDragonRenderer extends LatexHumanoidRenderer<LatexTrafficConeDragon, LatexTrafficConeDragonModel, ArmorLatexTrafficConeDragonModel<LatexTrafficConeDragon>> {
+public class LatexTrafficConeDragonRenderer extends LatexHumanoidRenderer<LatexTrafficConeDragon, LatexTrafficConeDragonModel, ArmorLatexBigTailDragonModel<LatexTrafficConeDragon>> {
 	public LatexTrafficConeDragonRenderer(EntityRendererProvider.Context context) {
 		super(context, new LatexTrafficConeDragonModel(context.bakeLayer(LatexTrafficConeDragonModel.LAYER_LOCATION)),
-				ArmorLatexTrafficConeDragonModel::new, ArmorLatexTrafficConeDragonModel.INNER_ARMOR, ArmorLatexTrafficConeDragonModel.OUTER_ARMOR, 0.5f);
+				ArmorLatexBigTailDragonModel::new, ArmorLatexBigTailDragonModel.INNER_ARMOR, ArmorLatexBigTailDragonModel.OUTER_ARMOR, 0.5f);
 		this.addLayer(new LatexParticlesLayer<>(this, this.model));
 		this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(),
 				CustomEyesLayer.fixedColor(Color3.parseHex("#ffea86")),
