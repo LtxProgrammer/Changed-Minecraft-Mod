1aa8f0a11a276fd29001a688577373a430875c5a
LtxPgm <ltxprogrammer@outlook.com>
2024-01-23 10:45:20 -0600

    Fixed grabbed entity renderer copying properties from the last mob renderer.

--- src/main/java/net/ltxprogrammer/changed/mixin/render/LivingEntityRendererMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/render/LivingEntityRendererMixin.java
@@ -28,18 +28,28 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
 
     @Shadow protected abstract float getWhiteOverlayProgress(T p_115334_, float p_115335_);
 
+    @Shadow protected M model;
+
+    @Shadow protected abstract float getAttackAnim(T p_115343_, float p_115344_);
+
     @Override
     public void directRender(T entity, float yRot, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+        this.model.attackTime = this.getAttackAnim(entity, partialTicks);
+        this.model.riding = false;
+        this.model.young = entity.isBaby();
+        this.model.setupAnim(entity, 0.0f, 0.0f, entity.tickCount + partialTicks, 0.0f, 0.0f);
+
         boolean bodyVisible = this.isBodyVisible(entity);
         boolean shouldBeVisible = !bodyVisible && !entity.isInvisibleTo(Minecraft.getInstance().player);
         boolean shouldGlow = Minecraft.getInstance().shouldEntityAppearGlowing(entity);
         var renderType = this.getRenderType(entity, bodyVisible, shouldBeVisible, shouldGlow);
-        int overlay = LivingEntityRenderer.getOverlayCoords(entity, this.getWhiteOverlayProgress(entity, partialTicks));
-        this.getModel().renderToBuffer(poseStack, bufferSource.getBuffer(renderType), packedLight, overlay, 1.0f, 1.0f, 1.0f, 1.0f);
+        if (renderType != null) {
+            int overlay = LivingEntityRenderer.getOverlayCoords(entity, this.getWhiteOverlayProgress(entity, partialTicks));
+            this.model.renderToBuffer(poseStack, bufferSource.getBuffer(renderType), packedLight, overlay, 1.0f, 1.0f, 1.0f, 1.0f);
+        }
 
-        for (Object layer : this.layers) {
-            if (!(layer instanceof RenderLayer renderLayer)) return;
-            renderLayer.render(poseStack, bufferSource, packedLight, entity, 0.0f, 0.0f, partialTicks, 0.0f, 0.0f, 0.0f);
+        for (var layer : this.layers) {
+            layer.render(poseStack, bufferSource, packedLight, entity, 0.0f, 0.0f, partialTicks, 0.0f, 0.0f, 0.0f);
         }
     }
 }
