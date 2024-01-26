39faaec7ff6f4a039786a967ad2f5025244d6826
Bijimio <mp39@tutanota.com>
2024-01-25 23:26:22 -0800

    Bijimio Experimental Fork V5.9.6 | Shaded Male Latex Squid Dog, Updated Translations

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSquidDogMaleRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSquidDogMaleRenderer.java
@@ -1,9 +1,12 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexSquidDogMaleModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexSquidDogModel;
 import net.ltxprogrammer.changed.entity.beast.LatexSquidDogMale;
+import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -11,6 +14,11 @@ public class LatexSquidDogMaleRenderer extends LatexHumanoidRenderer<LatexSquidD
     public LatexSquidDogMaleRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexSquidDogMaleModel(context.bakeLayer(LatexSquidDogMaleModel.LAYER_LOCATION)),
                 ArmorLatexSquidDogModel::new, ArmorLatexSquidDogModel.INNER_ARMOR, ArmorLatexSquidDogModel.OUTER_ARMOR, 0.65f);
+        this.addLayer(new LatexParticlesLayer<>(this, this.model));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(),
+                CustomEyesLayer.fixedColor(Color3.BLACK),
+                CustomEyesLayer.fixedColor(Color3.parseHex("#ffffff")),
+                CustomEyesLayer.fixedColor(Color3.parseHex("#ffffff"))));
     }
 
     @Override
