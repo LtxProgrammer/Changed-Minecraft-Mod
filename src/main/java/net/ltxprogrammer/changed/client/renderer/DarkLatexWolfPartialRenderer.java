93a8c0e52cb3f5177ee2414e1084d28dafefae3c
LtxPgm <ltxprogrammer@outlook.com>
2024-01-30 09:21:33 -0600

    Added slim model version to partial TF

--- src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexWolfPartialRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/DarkLatexWolfPartialRenderer.java
@@ -8,21 +8,22 @@ import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfM
 import net.ltxprogrammer.changed.entity.beast.DarkLatexWolfPartial;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.resources.ResourceLocation;
 
 public class DarkLatexWolfPartialRenderer extends LatexHumanoidRenderer<DarkLatexWolfPartial, DarkLatexWolfPartialModel, ArmorLatexMaleWolfModel<DarkLatexWolfPartial>> {
-	public DarkLatexWolfPartialRenderer(EntityRendererProvider.Context context) {
-		super(context, new DarkLatexWolfPartialModel(context.bakeLayer(DarkLatexWolfPartialModel.LAYER_LOCATION_HUMAN)),
+	public DarkLatexWolfPartialRenderer(EntityRendererProvider.Context context, boolean slim) {
+		super(context, new DarkLatexWolfPartialModel(context.bakeLayer(
+				slim ? DarkLatexWolfPartialModel.LAYER_LOCATION_HUMAN_SLIM : DarkLatexWolfPartialModel.LAYER_LOCATION_HUMAN)),
 				ArmorLatexMaleWolfModel::new, ArmorLatexMaleWolfModel.INNER_ARMOR, ArmorLatexMaleWolfModel.OUTER_ARMOR, 0.5f);
-		this.addLayer(new LatexPartialLayer<>(this, new DarkLatexWolfPartialModel(context.bakeLayer(DarkLatexWolfPartialModel.LAYER_LOCATION_LATEX)), Changed.modResource("textures/dark_latex_wolf_partial.png")));
+		this.addLayer(new LatexPartialLayer<>(this, new DarkLatexWolfPartialModel(
+				context.bakeLayer(slim ? DarkLatexWolfPartialModel.LAYER_LOCATION_LATEX_SLIM : DarkLatexWolfPartialModel.LAYER_LOCATION_LATEX)),
+				slim ? Changed.modResource("textures/dark_latex_wolf_partial_slim.png") : Changed.modResource("textures/dark_latex_wolf_partial.png")));
 		this.addLayer(new LatexParticlesLayer<>(this, getModel()));
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(DarkLatexWolfPartial partial) {
-		var underlying = partial.getUnderlyingPlayer();
-		if (!(underlying instanceof AbstractClientPlayer clientPlayer))
-			return Changed.modResource("textures/dark_latex_wolf_male.png"); // Fallback
-		return clientPlayer.getSkinTextureLocation();
+		return partial.getSkinTextureLocation();
 	}
 }
--- src/main/java/net/ltxprogrammer/changed/mixin/render/EntityRenderDispatcherMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/render/EntityRenderDispatcherMixin.java
@@ -2,14 +2,24 @@ package net.ltxprogrammer.changed.mixin.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.ltxprogrammer.changed.client.EntityRenderHelper;
+import net.ltxprogrammer.changed.init.ChangedEntityRenderers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
 
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin {
@@ -21,5 +31,18 @@ public abstract class EntityRenderDispatcherMixin {
     @Inject(method = "render", at = @At("RETURN"))
     public <E extends Entity> void afterRender(E entity, double camX, double camY, double camZ, float p_114389_, float p_114390_, PoseStack poseStack, MultiBufferSource buffers, int p_114393_, CallbackInfo callback) {
         EntityRenderHelper.ENTITY_RENDER_DISPATCHER_ENTITY_MINUS_CAMERA.removeLast();
+    }
+
+    @Inject(method = "getRenderer", at = @At("HEAD"), cancellable = true)
+    public <E extends Entity> void getRendererOrOverridden(E entity, CallbackInfoReturnable<EntityRenderer<? super E>> callback) {
+        EntityRenderer<? super E> override = ChangedEntityRenderers.getRenderer(entity);
+        if (override == null) return;
+        callback.setReturnValue(override);
+    }
+
+    @Redirect(method = "onResourceManagerReload", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/EntityRenderers;createEntityRenderers(Lnet/minecraft/client/renderer/entity/EntityRendererProvider$Context;)Ljava/util/Map;"))
+    public Map<EntityType<?>, EntityRenderer<?>> reloadChangedRenderers(EntityRendererProvider.Context context) {
+        ChangedEntityRenderers.registerComplexRenderers(context);
+        return EntityRenderers.createEntityRenderers(context);
     }
 }
