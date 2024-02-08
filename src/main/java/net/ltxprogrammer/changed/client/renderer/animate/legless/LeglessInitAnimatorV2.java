8e17d55dd153bc0c092a12e9c0f6f06bfed63f96
LtxPgm <ltxprogrammer@outlook.com>
2024-02-08 10:43:59 -0600

    Added initial x-position to legless init animator

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/legless/LeglessInitAnimatorV2.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/legless/LeglessInitAnimatorV2.java
@@ -26,6 +26,7 @@ public class LeglessInitAnimatorV2<T extends LatexEntity, M extends EntityModel<
     @Override
     public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         float slitherAmount = Math.min(0.5F, limbSwingAmount);
+        abdomen.x = 0.0f;
         abdomen.xRot = (float) Math.toRadians(-12.5);
         abdomen.yRot = Mth.cos(limbSwing * 0.33333334F + ((float)Math.PI / 2.0F)) * 0.6F * slitherAmount;
         abdomen.zRot = Mth.cos(limbSwing * 0.33333334F) * 0.6F * slitherAmount;
