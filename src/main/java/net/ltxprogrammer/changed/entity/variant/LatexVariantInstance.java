dea7bc02dac207500f7269ddaa1f1ec2e4b6e07c
LtxPgm <ltxprogrammer@outlook.com>
2024-02-24 08:47:01 -0600

    Fixed bug where untf'd aquatic players would have no gravity

--- src/main/java/net/ltxprogrammer/changed/entity/variant/LatexVariantInstance.java
+++ src/main/java/net/ltxprogrammer/changed/entity/variant/LatexVariantInstance.java
@@ -658,6 +658,7 @@ public class LatexVariantInstance<T extends LatexEntity> {
             player.onUpdateAbilities();
         }
         player.maxUpStep = 0.6F;
+        player.setNoGravity(false);
         player.refreshDimensions();
     }
 
