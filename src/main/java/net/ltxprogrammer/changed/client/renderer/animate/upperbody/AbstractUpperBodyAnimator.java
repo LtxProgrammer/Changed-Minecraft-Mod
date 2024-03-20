70a3eb42156a978f828563c85210349b98fe9a59
LtxPgm <ltxprogrammer@outlook.com>
2024-03-20 09:26:17 -0500

    Added compatibility with moonlight implemented animations (including supplementaries) for #821

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/upperbody/AbstractUpperBodyAnimator.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/upperbody/AbstractUpperBodyAnimator.java
@@ -3,10 +3,13 @@ package net.ltxprogrammer.changed.client.renderer.animate.upperbody;
 import net.ltxprogrammer.changed.client.renderer.animate.LatexAnimator;
 import net.ltxprogrammer.changed.entity.LatexEntity;
 import net.ltxprogrammer.changed.item.SpecializedAnimations;
+import net.minecraft.client.model.AnimationUtils;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.HumanoidArm;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractUpperBodyAnimator<T extends LatexEntity, M extends EntityModel<T>> extends LatexAnimator.Animator<T, M> {
     public final ModelPart head;
@@ -24,6 +27,77 @@ public abstract class AbstractUpperBodyAnimator<T extends LatexEntity, M extends
                 return controller.getArm(humanoidArm);
             }
         };
+    }
+
+    protected void poseRightArmForItem(T entity) {
+        switch(core.rightArmPose) {
+            case EMPTY:
+                rightArm.yRot = 0.0F;
+                break;
+            case BLOCK:
+                rightArm.xRot = rightArm.xRot * 0.5F - 0.9424779F;
+                rightArm.yRot = (-(float)Math.PI / 6F);
+                break;
+            case ITEM:
+                rightArm.xRot = rightArm.xRot * 0.5F - ((float)Math.PI / 10F);
+                rightArm.yRot = 0.0F;
+                break;
+            case THROW_SPEAR:
+                rightArm.xRot = rightArm.xRot * 0.5F - (float)Math.PI;
+                rightArm.yRot = 0.0F;
+                break;
+            case BOW_AND_ARROW:
+                rightArm.yRot = -0.1F + head.yRot;
+                leftArm.yRot = 0.1F + head.yRot + 0.4F;
+                rightArm.xRot = (-(float)Math.PI / 2F) + head.xRot;
+                leftArm.xRot = (-(float)Math.PI / 2F) + head.xRot;
+                break;
+            case CROSSBOW_CHARGE:
+                AnimationUtils.animateCrossbowCharge(rightArm, leftArm, entity, true);
+                break;
+            case CROSSBOW_HOLD:
+                AnimationUtils.animateCrossbowHold(rightArm, leftArm, head, true);
+                break;
+            case SPYGLASS:
+                rightArm.xRot = Mth.clamp(head.xRot - 1.9198622F - (entity.isCrouching() ? 0.2617994F : 0.0F), -2.4F, 3.3F);
+                rightArm.yRot = head.yRot - 0.2617994F;
+        }
+
+    }
+
+    protected void poseLeftArmForItem(T entity) {
+        switch(core.leftArmPose) {
+            case EMPTY:
+                leftArm.yRot = 0.0F;
+                break;
+            case BLOCK:
+                leftArm.xRot = leftArm.xRot * 0.5F - 0.9424779F;
+                leftArm.yRot = ((float)Math.PI / 6F);
+                break;
+            case ITEM:
+                leftArm.xRot = leftArm.xRot * 0.5F - ((float)Math.PI / 10F);
+                leftArm.yRot = 0.0F;
+                break;
+            case THROW_SPEAR:
+                leftArm.xRot = leftArm.xRot * 0.5F - (float)Math.PI;
+                leftArm.yRot = 0.0F;
+                break;
+            case BOW_AND_ARROW:
+                rightArm.yRot = -0.1F + head.yRot - 0.4F;
+                leftArm.yRot = 0.1F + head.yRot;
+                rightArm.xRot = (-(float)Math.PI / 2F) + head.xRot;
+                leftArm.xRot = (-(float)Math.PI / 2F) + head.xRot;
+                break;
+            case CROSSBOW_CHARGE:
+                AnimationUtils.animateCrossbowCharge(rightArm, leftArm, entity, true);
+                break;
+            case CROSSBOW_HOLD:
+                AnimationUtils.animateCrossbowHold(rightArm, leftArm, head, true);
+                break;
+            case SPYGLASS:
+                leftArm.xRot = Mth.clamp(head.xRot - 1.9198622F - (entity.isCrouching() ? 0.2617994F : 0.0F), -2.4F, 3.3F);
+                leftArm.yRot = head.yRot + 0.2617994F;
+        }
     }
 
     protected ModelPart getArm(HumanoidArm humanoidArm) {
@@ -52,4 +126,8 @@ public abstract class AbstractUpperBodyAnimator<T extends LatexEntity, M extends
         this.leftArm.copyFrom(humanoidModel.leftArm);
         this.rightArm.copyFrom(humanoidModel.rightArm);
     }
+
+    // Hook for moonlight mixin
+    @Override
+    public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {}
 }
---
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/Moonlight/AbstractUpperBodyAnimatorMixin.java
@@ -0,0 +1,62 @@
+package net.ltxprogrammer.changed.mixin.compatibility.Moonlight;
+
+import net.ltxprogrammer.changed.client.renderer.animate.LatexAnimator;
+import net.ltxprogrammer.changed.client.renderer.animate.upperbody.AbstractUpperBodyAnimator;
+import net.ltxprogrammer.changed.entity.LatexEntity;
+import net.mehvahdjukaar.selene.api.IThirdPersonAnimationProvider;
+import net.mehvahdjukaar.selene.util.TwoHandedAnimation;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = AbstractUpperBodyAnimator.class, remap = false)
+public abstract class AbstractUpperBodyAnimatorMixin<T extends LatexEntity, M extends EntityModel<T>> extends LatexAnimator.Animator<T, M> {
+    @Unique
+    public TwoHandedAnimation animationType = new TwoHandedAnimation();
+
+    @Inject(method = "poseRightArmForItem", at = @At("HEAD"), cancellable = true, require = 0)
+    public void poseRightArm(T entity, CallbackInfo ci) {
+        //cancel off hand animation if two handed so two handed animation always happens last
+        if (this.animationType.isTwoHanded()) ci.cancel();
+        HumanoidArm handSide = entity.getMainArm();
+        ItemStack stack = entity.getItemInHand(handSide == HumanoidArm.RIGHT ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND);
+        Item item = stack.getItem();
+        if (item instanceof IThirdPersonAnimationProvider thirdPersonAnimationProvider) {
+            HumanoidModel propertyModel = this.core.getPropertyModel(null);
+            if (thirdPersonAnimationProvider.poseRightArm(stack, propertyModel, entity, handSide, this.animationType)) {
+                this.core.applyPropertyModel(propertyModel);
+                ci.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "poseLeftArmForItem", at = @At(value = "HEAD"), cancellable = true, require = 0)
+    public void poseLeftArm(T entity, CallbackInfo ci) {
+        //cancel off hand animation if two handed so two handed animation always happens last
+        if (this.animationType.isTwoHanded()) ci.cancel();
+        HumanoidArm handSide = entity.getMainArm();
+        ItemStack stack = entity.getItemInHand(handSide == HumanoidArm.RIGHT ? InteractionHand.OFF_HAND : InteractionHand.MAIN_HAND);
+        Item item = stack.getItem();
+        if (item instanceof IThirdPersonAnimationProvider thirdPersonAnimationProvider) {
+            HumanoidModel propertyModel = this.core.getPropertyModel(null);
+            if (thirdPersonAnimationProvider.poseLeftArmGeneric(stack, propertyModel, entity, handSide, this.animationType)) {
+                this.core.applyPropertyModel(propertyModel);
+                ci.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "setupAnim", at = @At(value = "RETURN"), require = 0)
+    public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        this.animationType.setTwoHanded(false);
+    }
+}
