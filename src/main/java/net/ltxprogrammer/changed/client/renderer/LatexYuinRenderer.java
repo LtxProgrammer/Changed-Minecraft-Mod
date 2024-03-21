660f0fd18710c7287b4e566aae90bee7b6645fa8
Bijimio <mp39@tutanota.com>
2024-03-21 04:14:42 +0100

    Bijimio Experimental Fork V6.3.0 | Created Deer Armor Model, Updated Lang

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexYuinRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexYuinRenderer.java
@@ -5,16 +5,16 @@ import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexYuinModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexYuinModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexDeerModel;
 import net.ltxprogrammer.changed.entity.beast.LatexYuin;
 import net.ltxprogrammer.changed.util.Color3;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexYuinRenderer extends LatexHumanoidRenderer<LatexYuin, LatexYuinModel, ArmorLatexYuinModel<LatexYuin>> {
+public class LatexYuinRenderer extends LatexHumanoidRenderer<LatexYuin, LatexYuinModel, ArmorLatexDeerModel<LatexYuin>> {
     public LatexYuinRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexYuinModel(context.bakeLayer(LatexYuinModel.LAYER_LOCATION)),
-                ArmorLatexYuinModel::new, ArmorLatexYuinModel.INNER_ARMOR, ArmorLatexYuinModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexDeerModel::new, ArmorLatexDeerModel.INNER_ARMOR, ArmorLatexDeerModel.OUTER_ARMOR, 0.5f);
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer.fixedColor(Color3.WHITE),CustomEyesLayer.fixedColor(Color3.parseHex("#ffc301"))));
         this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
