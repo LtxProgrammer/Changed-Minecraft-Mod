e1e6bb87b7b465eb9d3ef5e99b237a42027b7877
LtxPgm <ltxprogrammer@outlook.com>
2024-03-21 10:59:38 -0500

    Fixed breastplate on female armors being too low

--- src/main/java/net/ltxprogrammer/changed/client/renderer/model/armor/ArmorLatexFemaleSquidDogModel.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/armor/ArmorLatexFemaleSquidDogModel.java
@@ -98,7 +98,7 @@ public class ArmorLatexFemaleSquidDogModel<T extends LatexEntity> extends LatexH
 
         PartDefinition Torso = partdefinition.addOrReplaceChild("Torso", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.1F, -2.2F, 8.0F, 12.0F, 4.0F, layer.dualDeformation.extend(0.1F)), PartPose.offset(0.0F, -2.2F, 0.0F));
 
-        PartDefinition Plantoids = Torso.addOrReplaceChild("Plantoids", CubeListBuilder.create(), PartPose.offset(0.0F, 1.0F, -2.2F));
+        PartDefinition Plantoids = Torso.addOrReplaceChild("Plantoids", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, -2.2F));
 
         PartDefinition Plantoid_r1 = Plantoids.addOrReplaceChild("Plantoid_r1", CubeListBuilder.create().texOffs(18, 22).mirror().addBox(-4.0F, 2.3F, -0.8F, 8.0F, 1.0F, 2.0F, layer.deformation.extend(-0.5f)).mirror(false)
                 .texOffs(18, 19).mirror().addBox(-4.0F, -1.7F, -0.8F, 8.0F, 3.0F, 2.0F, layer.deformation.extend(-0.5f)).mirror(false), PartPose.offsetAndRotation(0.0F, 2.0F, 0.0F, -0.2793F, 0.0F, 0.0F));
