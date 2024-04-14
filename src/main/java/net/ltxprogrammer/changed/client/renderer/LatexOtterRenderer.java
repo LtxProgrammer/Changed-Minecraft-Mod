73a8a472aca1a1224e3af604013169d9598b2d9e
Bijimio <mp39@tutanota.com>
2024-04-14 23:49:50 +0200

    Bijimio Experimental Fork V6.3.9 | Modeled and Implemented Unified Latex Otter

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexOtterRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexOtterRenderer.java
@@ -1,7 +1,10 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexOtterModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexOtterModel;
 import net.ltxprogrammer.changed.entity.beast.LatexOtter;
@@ -12,7 +15,10 @@ public class LatexOtterRenderer extends LatexHumanoidRenderer<LatexOtter, LatexO
     public LatexOtterRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexOtterModel(context.bakeLayer(LatexOtterModel.LAYER_LOCATION)),
                 ArmorLatexOtterModel::new, ArmorLatexOtterModel.INNER_ARMOR, ArmorLatexOtterModel.OUTER_ARMOR, 0.5f);
-        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
+        this.addLayer(new LatexParticlesLayer<>(this, this.model));
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
+        this.addLayer(GasMaskLayer.forLargeSnouted(this, context.getModelSet()));
     }
 
     @Override
