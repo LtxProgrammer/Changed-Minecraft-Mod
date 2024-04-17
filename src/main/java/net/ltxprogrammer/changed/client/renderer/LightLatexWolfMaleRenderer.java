d5ccda770998c6b4633beae2db485b87cb53f0fe
Bijimio <mp39@tutanota.com>
2024-04-17 19:05:49 +0200

    Bijimio Experimental Fork V6.4.4 | Fixed Missing Gas Mask Layer For Light Latex Wolf

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LightLatexWolfMaleRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LightLatexWolfMaleRenderer.java
@@ -2,6 +2,7 @@ package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LightLatexWolfMaleModel;
@@ -17,6 +18,7 @@ public class LightLatexWolfMaleRenderer extends AdvancedHumanoidRenderer<LightLa
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
         this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
+        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
     }
 
     @Override
