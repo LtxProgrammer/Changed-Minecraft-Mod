e4e07b85040d8fbf3379cff7f639379b6e9283c1
LtxPgm <ltxprogrammer@outlook.com>
2024-05-20 12:35:36 -0500

    Updated fluid to damage hostile factions

--- src/main/java/net/ltxprogrammer/changed/fluid/AbstractLatexFluid.java
+++ src/main/java/net/ltxprogrammer/changed/fluid/AbstractLatexFluid.java
@@ -6,6 +6,7 @@ import net.ltxprogrammer.changed.entity.LatexType;
 import net.ltxprogrammer.changed.entity.TransfurCause;
 import net.ltxprogrammer.changed.entity.TransfurContext;
 import net.ltxprogrammer.changed.entity.variant.TransfurVariant;
+import net.ltxprogrammer.changed.init.ChangedDamageSources;
 import net.ltxprogrammer.changed.init.ChangedTags;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.core.BlockPos;
@@ -67,14 +68,13 @@ public abstract class AbstractLatexFluid extends ForgeFlowingFluid {
                 event.getEntityLiving().makeStuckInBlock(state, new Vec3(0.75, 0.75, 0.75));
         }
 
-        if (event.getEntityLiving() instanceof Player player && ProcessTransfur.isPlayerLatex(player))
-            return;
-        if (event.getEntityLiving() instanceof ChangedEntity)
-            return;
-
         if (event.getEntityLiving().isAlive() && !event.getEntityLiving().isDeadOrDying() && fluid != null) {
-            ProcessTransfur.progressTransfur(event.getEntityLiving(), 5.0f, fluid.form.get(level.random.nextInt(fluid.form.size())),
-                    TransfurContext.hazard(TransfurCause.GRAB_REPLICATE));
+            TransfurVariant<?> variant = TransfurVariant.getEntityVariant(event.getEntityLiving());
+            if (variant == null)
+                ProcessTransfur.progressTransfur(event.getEntityLiving(), 5.0f, fluid.form.get(level.random.nextInt(fluid.form.size())),
+                        TransfurContext.hazard(TransfurCause.LATEX_PUDDLE));
+            else if (variant.getLatexType().isHostileTo(fluid.gooType))
+                event.getEntityLiving().hurt(ChangedDamageSources.LATEX_FLUID, 2.0f);
         }
     }
 
