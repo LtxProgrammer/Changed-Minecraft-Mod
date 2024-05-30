bb2256a66142b6e0253e170f6ea5e475f5544d01
LtxPgm <ltxprogrammer@outlook.com>
2024-05-30 10:15:31 -0500

    Added basic compatibility with beyond earth. Fixed #908.

---
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/BeyondEarth/SpaceSuitModelP2Mixin.java
@@ -0,0 +1,23 @@
+package net.ltxprogrammer.changed.mixin.compatibility.BeyondEarth;
+
+import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModelInterface;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import net.mrscauthd.beyond_earth.entities.renderer.spacesuit.SpaceSuitModel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = SpaceSuitModel.SPACE_SUIT_P2.class, remap = false)
+public abstract class SpaceSuitModelP2Mixin {
+    @Redirect(method = "renderToBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/LivingEntityRenderer;getModel()Lnet/minecraft/client/model/EntityModel;"))
+    public EntityModel<? extends LivingEntity> getHumanoidModel(LivingEntityRenderer<LivingEntity, ?> instance) {
+        var model = instance.getModel();
+        if (model instanceof AdvancedHumanoidModelInterface<?, ?> changedModel) {
+            return changedModel.getAnimator().getPropertyModel(null);
+        }
+
+        return model;
+    }
+}
