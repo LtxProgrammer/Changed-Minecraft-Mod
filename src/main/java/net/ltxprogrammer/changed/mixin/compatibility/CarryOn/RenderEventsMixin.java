5595d60938a6c8180b7c24cab5b8904fd1ebbfa2
LtxPgm <ltxprogrammer@outlook.com>
2024-06-01 18:23:20 -0500

    Added item offset for #917. Fixed model setup for copying torso offset.

--- src/main/java/net/ltxprogrammer/changed/mixin/compatibility/CarryOn/RenderEventsMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/CarryOn/RenderEventsMixin.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.datafixers.util.Pair;
 import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
+import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModel;
 import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModelInterface;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.client.Minecraft;
@@ -14,11 +15,17 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,8 +33,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tschipp.carryon.client.event.RenderEvents;
+import tschipp.carryon.client.helper.CarryRenderHelper;
 
 import java.util.List;
 import java.util.Map;
@@ -85,6 +94,73 @@ public abstract class RenderEventsMixin {
     public void renderChangedArmPost(ModelPart arm, float x, float z, boolean right, boolean sneaking, int light, PoseStack matrix, VertexConsumer builder, CallbackInfo ci) {
         remapHumanoidPart(arm).ifPresent(pair -> {
             ci.cancel();
+        });
+    }
+
+    @Redirect(method = "onRenderLevel", at = @At(value = "INVOKE", target = "Ltschipp/carryon/client/helper/CarryRenderHelper;renderItem(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/resources/model/BakedModel;)V"))
+    public void maybeOffsetItem(BlockState state, CompoundTag tag, ItemStack stack, ItemStack tileStack, PoseStack matrix, MultiBufferSource buffer, int light, BakedModel model) {
+        ProcessTransfur.ifPlayerTransfurred(currentPlayer, variant -> {
+            matrix.pushPose(); // reset matrix to entity
+            EntityRenderer renderer = Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(variant.getChangedEntity());
+            if (renderer instanceof AdvancedHumanoidRenderer<?,?,?> advanced) {
+                int perspective = CarryRenderHelper.getPerspective();
+                var entity = variant.getChangedEntity();
+                AdvancedHumanoidModel entityModel = advanced.getModel(entity);
+                float partialTicks = Minecraft.getInstance().getDeltaFrameTime();
+
+                boolean shouldSit = entity.isPassenger() && (entity.getVehicle() != null && entity.getVehicle().shouldRiderSit());
+                float f = Mth.rotLerp(partialTicks, entity.yBodyRotO, entity.yBodyRot);
+                float f1 = Mth.rotLerp(partialTicks, entity.yHeadRotO, entity.yHeadRot);
+                float netHeadYaw = f1 - f;
+                if (shouldSit && entity.getVehicle() instanceof LivingEntity) {
+                    LivingEntity livingentity = (LivingEntity)entity.getVehicle();
+                    f = Mth.rotLerp(partialTicks, livingentity.yBodyRotO, livingentity.yBodyRot);
+                    netHeadYaw = f1 - f;
+                    float f3 = Mth.wrapDegrees(netHeadYaw);
+                    if (f3 < -85.0F) {
+                        f3 = -85.0F;
+                    }
+
+                    if (f3 >= 85.0F) {
+                        f3 = 85.0F;
+                    }
+
+                    f = f1 - f3;
+                    if (f3 * f3 > 2500.0F) {
+                        f += f3 * 0.2F;
+                    }
+
+                    netHeadYaw = f1 - f;
+                }
+
+                float headPitch = Mth.lerp(partialTicks, entity.xRotO, entity.getXRot());
+                if (LivingEntityRenderer.isEntityUpsideDown(entity)) {
+                    headPitch *= -1.0F;
+                    netHeadYaw *= -1.0F;
+                }
+
+                float limbSwing = entity.animationPosition - entity.animationSpeed * (1.0F - partialTicks);
+                float limbSwingAmount = Math.min(Mth.lerp(partialTicks, entity.animationSpeedOld, entity.animationSpeed), 1.0F);
+
+                // Setup animations for the entity
+                entityModel.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTicks);
+                entityModel.setupAnim(entity, limbSwing, limbSwingAmount, entity.tickCount + partialTicks, netHeadYaw, headPitch);
+                var torso = entityModel.getTorso();
+
+                if (entityModel instanceof AdvancedHumanoidModelInterface<?,?> modelInterface)
+                    matrix.translate(0.0, 0.0, (perspective == 2 ? -1 : 1) * modelInterface.getAnimator().forwardOffset / 8.0D);
+
+                if (torso != null) {
+                    float oldY = torso.y;
+                    torso.y = -torso.y;
+                    torso.translateAndRotate(matrix);
+                    torso.y = oldY;
+                }
+            }
+            CarryRenderHelper.renderItem(state, tag, stack, tileStack, matrix, buffer, light, model);
+            matrix.popPose();
+        }, () -> {
+            CarryRenderHelper.renderItem(state, tag, stack, tileStack, matrix, buffer, light, model);
         });
     }
 }
