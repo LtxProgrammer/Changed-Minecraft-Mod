c1ede9b84ed55d9fc09285b61089c74f8984be51
Bijimio <mp39@tutanota.com>
2024-06-03 05:11:06 +0200

    Bijimio Experimental Fork V6.6.5 | Created Female Manta Ray Armor, Adjusted Armor Layers.

--- src/main/java/net/ltxprogrammer/changed/client/renderer/model/armor/ArmorSirenAbdomenModel.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/armor/ArmorSirenAbdomenModel.java
@@ -47,17 +47,17 @@ public class ArmorSirenAbdomenModel<T extends ChangedEntity> extends LatexHumano
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition Abdomen = partdefinition.addOrReplaceChild("Abdomen", CubeListBuilder.create().texOffs(40, 0).addBox(-4.0F, -8.0F, -2.0F, 8.0F, 12.0F, 4.0F, layer.deformation), PartPose.offset(0.0F, 8.0F, 0.5F));
+        PartDefinition Abdomen = partdefinition.addOrReplaceChild("Abdomen", CubeListBuilder.create().texOffs(40, 7).addBox(-4.0F, -1.0F, -2.0F, 8.0F, 5.0F, 4.0F, layer.deformation), PartPose.offset(0.0F, 8.0F, 0.5F));
 
-        PartDefinition LowerAbdomen = Abdomen.addOrReplaceChild("LowerAbdomen", CubeListBuilder.create().texOffs(0, 8).addBox(-4.5F, -2.0F, -1.75F, 9.0F, 7.0F, 5.0F, layer.deformation), PartPose.offset(0.0F, 4.5F, -0.75F));
+        PartDefinition LowerAbdomen = Abdomen.addOrReplaceChild("LowerAbdomen", CubeListBuilder.create().texOffs(0, 8).addBox(-4.5F, -1.5F, -1.75F, 9.0F, 7.0F, 5.0F, layer.deformation), PartPose.offset(0.0F, 4.5F, -0.75F));
 
         PartDefinition Tail = LowerAbdomen.addOrReplaceChild("Tail", CubeListBuilder.create().texOffs(0, 20).addBox(-4.0F, -0.75F, -2.0F, 8.0F, 6.0F, 4.0F, layer.altDeformation.extend(0.65F)), PartPose.offset(0.0F, 6.0F, 0.5F));
 
-        PartDefinition TailPrimary = Tail.addOrReplaceChild("TailPrimary", CubeListBuilder.create().texOffs(31, 0).addBox(-3.5F, -0.25F, -2.0F, 7.0F, 5.0F, 4.0F, layer.altDeformation.extend(0.26F)), PartPose.offset(0.0F, 4.75F, 0.0F));
+        PartDefinition TailPrimary = Tail.addOrReplaceChild("TailPrimary", CubeListBuilder.create().texOffs(40, 21).addBox(-3.5F, -0.25F, -2.0F, 7.0F, 5.0F, 4.0F, layer.altDeformation.extend(0.26F)), PartPose.offset(0.0F, 4.75F, 0.0F));
 
         PartDefinition TailSecondary = TailPrimary.addOrReplaceChild("TailSecondary", CubeListBuilder.create().texOffs(24, 16).addBox(-3.0F, -0.25F, -2.0F, 6.0F, 5.0F, 4.0F, layer.altDeformation), PartPose.offset(0.0F, 4.5F, 0.0F));
 
-        PartDefinition TailTertiary = TailSecondary.addOrReplaceChild("TailTertiary", CubeListBuilder.create().texOffs(44, 17).addBox(-2.0F, -0.25F, -1.5F, 4.0F, 3.0F, 3.0F, layer.altDeformation.extend(0.15F)), PartPose.offset(0.0F, 4.75F, 0.0F));
+        PartDefinition TailTertiary = TailSecondary.addOrReplaceChild("TailTertiary", CubeListBuilder.create().texOffs(44, 0).addBox(-2.0F, -0.25F, -1.5F, 4.0F, 3.0F, 3.0F, layer.altDeformation.extend(0.15F)), PartPose.offset(0.0F, 4.75F, 0.0F));
 
         PartDefinition TailQuaternary = TailTertiary.addOrReplaceChild("TailQuaternary", CubeListBuilder.create().texOffs(24, 25).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 4.0F, 3.0F, layer.slightAltDeformation), PartPose.offset(0.0F, 2.55F, 0.0F));
 
