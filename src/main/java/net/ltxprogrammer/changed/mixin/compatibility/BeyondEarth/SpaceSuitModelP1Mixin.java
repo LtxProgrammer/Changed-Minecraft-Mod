9b1fc38e811ac1b0c17e8564e6279df646b489ac
LtxPgm <ltxprogrammer@outlook.com>
2024-06-05 09:55:14 -0500

    Reversion beyond earth to 7.0, fixed #923

--- src/main/java/net/ltxprogrammer/changed/mixin/compatibility/BeyondEarth/SpaceSuitModelP1Mixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/BeyondEarth/SpaceSuitModelP1Mixin.java
@@ -1,15 +1,15 @@
 package net.ltxprogrammer.changed.mixin.compatibility.BeyondEarth;
 
+import com.st0x0ef.beyond_earth.client.renderers.armors.SpaceSuitModel;
 import net.ltxprogrammer.changed.client.renderer.model.AdvancedHumanoidModelInterface;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.LivingEntity;
-import net.mrscauthd.beyond_earth.entities.renderer.spacesuit.SpaceSuitModel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = SpaceSuitModel.SPACE_SUIT_P1.class, remap = false)
+@Mixin(value = SpaceSuitModel.SpaceSuitP1.class, remap = false)
 public abstract class SpaceSuitModelP1Mixin {
     @Redirect(method = "renderToBuffer", remap = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/LivingEntityRenderer;getModel()Lnet/minecraft/client/model/EntityModel;", remap = true))
     public EntityModel<? extends LivingEntity> getHumanoidModel(LivingEntityRenderer<LivingEntity, ?> instance) {
