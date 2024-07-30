9f66458c86582d802289018c1ed90d4e1ece83db
Bijimio <mp39@tutanota.com>
2024-07-30 23:42:44 +0200

    Bijimio Experimental Fork V6.8.5 | Modeled Unified Latex Squirrel

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSquirrelRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexSquirrelRenderer.java
@@ -1,17 +1,23 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
 import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
 import net.ltxprogrammer.changed.client.renderer.model.LatexSquirrelModel;
-import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorNoTailModel;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
 import net.ltxprogrammer.changed.entity.beast.LatexSquirrel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class LatexSquirrelRenderer extends AdvancedHumanoidRenderer<LatexSquirrel, LatexSquirrelModel, ArmorNoTailModel<LatexSquirrel>> {
+public class LatexSquirrelRenderer extends AdvancedHumanoidRenderer<LatexSquirrel, LatexSquirrelModel, ArmorLatexMaleWolfModel<LatexSquirrel>> {
     public LatexSquirrelRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexSquirrelModel(context.bakeLayer(LatexSquirrelModel.LAYER_LOCATION)),
-                ArmorNoTailModel::new, ArmorNoTailModel.INNER_ARMOR, ArmorNoTailModel.OUTER_ARMOR, 0.5f);
+                ArmorLatexMaleWolfModel::new, ArmorLatexMaleWolfModel.INNER_ARMOR, ArmorLatexMaleWolfModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet()));
         this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
     }
 
