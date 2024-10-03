560b79aa87a7c751e854bc4bf5611e8449d31947
LtxPgm <ltxprogrammer@outlook.com>
2024-10-03 13:18:42 -0500

    Fixed model offset with CGM

--- src/main/java/net/ltxprogrammer/changed/mixin/compatibility/CGM/HumanoidAnimatorMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/CGM/HumanoidAnimatorMixin.java
@@ -1,5 +1,6 @@
 package net.ltxprogrammer.changed.mixin.compatibility.CGM;
 
+import com.mojang.math.Vector3f;
 import com.mrcrayfish.guns.client.handler.AimingHandler;
 import com.mrcrayfish.guns.common.Gun;
 import com.mrcrayfish.guns.item.GunItem;
@@ -10,6 +11,7 @@ import net.ltxprogrammer.changed.entity.variant.TransfurVariantInstance;
 import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
@@ -19,6 +21,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -50,8 +53,29 @@ public abstract class HumanoidAnimatorMixin<T extends ChangedEntity, M extends A
             }
 
             Gun gun = gunItem.getModifiedGun(heldItem);
+            Vector3f rightArmPosO = getPartPos(model.rightArm);
+            Vector3f leftArmPosO = getPartPos(model.leftArm);
+            Vector3f headPosO = getPartPos(model.head);
+            model.rightArm.setPos(-5.0f, 2.0f, 0.0f);
+            model.leftArm.setPos(5.0f, 2.0f, 0.0f);
+            model.head.setPos(0.0f, 0.0f, 0.0f);
             gun.getGeneral().getGripType().getHeldAnimation().applyPlayerModelRotation(player, model.rightArm, model.leftArm, model.head, InteractionHand.MAIN_HAND, AimingHandler.get().getAimProgress(player, Minecraft.getInstance().getFrameTime()));
+            offsetPart(model.rightArm, rightArmPosO, new Vector3f(-5.0f, 2.0f, 0.0f));
+            offsetPart(model.leftArm, leftArmPosO, new Vector3f(5.0f, 2.0f, 0.0f));
+            offsetPart(model.head, headPosO, Vector3f.ZERO);
             this.applyPropertyModel(model);
         }
+    }
+
+    @Unique
+    private static Vector3f getPartPos(ModelPart part) {
+        return new Vector3f(part.x, part.y, part.z);
+    }
+
+    @Unique
+    protected void offsetPart(ModelPart part, Vector3f oldPos, Vector3f expectedPos) {
+        part.x = oldPos.x() + (expectedPos.x() - part.x);
+        part.y = oldPos.y() + (expectedPos.y() - part.y);
+        part.z = oldPos.z() + (expectedPos.z() - part.z);
     }
 }
