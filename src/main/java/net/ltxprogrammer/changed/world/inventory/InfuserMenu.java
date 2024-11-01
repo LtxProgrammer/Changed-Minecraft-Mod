28cdf9f0b91e21df78add9c20181042b791486de
Foxyas <119010731+Foxyas@users.noreply.github.com>
2024-11-01 16:07:43 -0300

    Update InfuserMenu.java
    Maybe Fixed The Latex Flask Recipe

--- src/main/java/net/ltxprogrammer/changed/world/inventory/InfuserMenu.java
+++ src/main/java/net/ltxprogrammer/changed/world/inventory/InfuserMenu.java
@@ -204,7 +204,7 @@ public class InfuserMenu extends RecipeBookMenu<SimpleContainer> implements Supp
         this.customSlots.put(SLOT_INPUT, this.addSlot(new InfuserSlotItemHandler(this, internal, 1, 18, 33) {
             @Override
             public boolean mayPlace(ItemStack stack) {
-                return stack.is(ChangedItems.BLOOD_SYRINGE.get()) || stack.is(Items.ARROW) || stack.is(Items.GLASS_BOTTLE);
+                return stack.is(ChangedItems.BLOOD_SYRINGE.get()) || stack.is(Items.ARROW) || stack.is(ChangedItems.getBlockItem(ChangedBlocks.ERLENMEYER_FLASK.get());
             }
         }));
 
