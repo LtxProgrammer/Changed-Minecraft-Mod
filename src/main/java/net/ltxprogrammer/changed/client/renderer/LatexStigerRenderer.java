41d8f7fe23d17047c64f08692b290de6769a6da6
LtxPgm <ltxprogrammer@outlook.com>
2024-11-24 19:32:14 -0600

    Added render layer for #1185

--- src/main/java/net/ltxprogrammer/changed/client/renderer/LatexStigerRenderer.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/LatexStigerRenderer.java
@@ -1,10 +1,7 @@
 package net.ltxprogrammer.changed.client.renderer;
 
 import net.ltxprogrammer.changed.Changed;
-import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
-import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
-import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
-import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.*;
 import net.ltxprogrammer.changed.client.renderer.model.LatexStigerModel;
 import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexStigerModel;
 import net.ltxprogrammer.changed.entity.beast.LatexStiger;
@@ -16,6 +13,7 @@ public class LatexStigerRenderer extends AdvancedHumanoidRenderer<LatexStiger, L
     public LatexStigerRenderer(EntityRendererProvider.Context context) {
         super(context, new LatexStigerModel(context.bakeLayer(LatexStigerModel.LAYER_LOCATION)),
                 ArmorLatexStigerModel::new, ArmorLatexStigerModel.INNER_ARMOR, ArmorLatexStigerModel.OUTER_ARMOR, 0.5f);
+        this.addLayer(new DoubleItemInHandLayer<>(this));
         this.addLayer(new LatexParticlesLayer<>(this, getModel()));
         this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
         this.addLayer(CustomEyesLayer.builder(this, context.getModelSet())
