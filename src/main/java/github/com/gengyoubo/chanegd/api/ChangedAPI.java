11ab3ae486ee0fc2314408d1950b3c54d21635c3
gengyoubo <gengyoubo@gmail.com>
2024-11-29 22:38:31 +0900

    update file

--- src/main/java/github/com/gengyoubo/chanegd/api/ChangedAPI.java
+++ src/main/java/github/com/gengyoubo/chanegd/api/ChangedAPI.java
@@ -2,9 +2,11 @@ package github.com.gengyoubo.chanegd.api;
 
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.entity.variant.TransfurVariant;
+import net.ltxprogrammer.changed.process.ProcessTransfur;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -13,13 +15,13 @@ public class ChangedAPI {
     /**
      * @return will return the latex variant of the current entity.
      */
-    public static TransfurVariant<?> getEntityLatexVariant(LivingEntity entity) {
+    public static TransfurVariant<?> getEntityLatexVariant(@NotNull LivingEntity entity) {
         return TransfurVariant.getEntityVariant(entity);
     }
     /**
      * @return will return the player's latex variant.
      */
-    public static TransfurVariant<?> getPlayerLatexVariant(ServerPlayer player) {
+    public static TransfurVariant<?> getPlayerLatexVariant(@NotNull Player player) {
         return getEntityLatexVariant(player);
     }
     /**
@@ -27,12 +29,18 @@ public class ChangedAPI {
      * @param variant Latex variants that need to be compared
      * @return Returns true if the player variant is the same as the latex variant being compared, otherwise it returns false.
      */
-    public static boolean isPlayerLatexVariantAppointed(@NotNull ServerPlayer player,@NotNull String variant) {
+    public static boolean isPlayerLatexVariantAppointed(@NotNull Player player,@NotNull String variant) {
         ResourceKey<?> importVariantKey = ResourceKey.createRegistryKey(Changed.modResource(variant));
         TransfurVariant<?> nowVariant = getPlayerLatexVariant(player);
         if (nowVariant == null || nowVariant.getRegistryName() == null) {
             return false;
         }
         return Objects.requireNonNull(nowVariant.getRegistryName()).equals(importVariantKey.location());
+    }
+    /**
+     * @return Checks if the player is a latex, returns true if it is otherwise returns false.
+     */
+    public static boolean isPlayerLatex(@NotNull Player player){
+        return getPlayerLatexVariant(player) != null;
     }
 }
