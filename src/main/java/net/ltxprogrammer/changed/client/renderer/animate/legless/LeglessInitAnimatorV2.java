a6752375911002fd9c895a5e5191608a34fc240d
LtxPgm <ltxprogrammer@outlook.com>
2025-02-14 17:30:07 -0600

    Revamped legless walk animation

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/legless/LeglessInitAnimatorV2.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/legless/LeglessInitAnimatorV2.java
@@ -14,6 +14,11 @@ import java.util.List;
  * @param <T>
  */
 public class LeglessInitAnimatorV2<T extends ChangedEntity, M extends AdvancedHumanoidModel<T>> extends AbstractLeglessAnimator<T, M> {
+    public static float SLITHER_AMOUNT = 0.9f;
+    public static float SLITHER_CURVE = 0.6f;
+    public static float SEGMENT_SIZE = 1.7f;
+    public static float SLITHER_RATE = 0.5f;
+
     public LeglessInitAnimatorV2(ModelPart abdomen, ModelPart lowerAbdomen, ModelPart tail, List<ModelPart> tailJoints) {
         super(abdomen, lowerAbdomen, tail, tailJoints);
     }
@@ -23,27 +28,48 @@ public class LeglessInitAnimatorV2<T extends ChangedEntity, M extends AdvancedHu
         return HumanoidAnimator.AnimateStage.INIT;
     }
 
+    public float slitherAnim(float segmentPosition, float time) {
+        return SLITHER_AMOUNT * Mth.sin(SLITHER_CURVE * segmentPosition - time) * (Math.min(segmentPosition, 2 * SEGMENT_SIZE) / (2 * SEGMENT_SIZE));
+    }
+
+    public float slitherAngle(float segmentPosition, float time, float lastPosition) {
+        return (slitherAnim(segmentPosition, time) - lastPosition) / SEGMENT_SIZE;
+    }
+
+    public float slitherAngle(float segmentPosition, float time) {
+        return slitherAngle(segmentPosition, time, slitherAnim(segmentPosition - SEGMENT_SIZE, time));
+    }
+
+    public float slitherDeltaAngle(float segmentPosition, float time) {
+        var x0 = slitherAnim(segmentPosition, time);
+        var x1 = slitherAnim(segmentPosition - SEGMENT_SIZE, time);
+        var x2 = slitherAnim(segmentPosition - 2 * SEGMENT_SIZE, time);
+
+        return ((x0 - x1) - (x1 - x2)) / SEGMENT_SIZE;
+    }
+
     @Override
     public void setupAnim(@NotNull T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        float slitherAmount = Math.min(0.5F, limbSwingAmount);
-        abdomen.x = 0.0f;
+        float overallAngle = 0f;
+        float segmentPosition = SEGMENT_SIZE;
         abdomen.xRot = (float) Math.toRadians(-12.5);
-        abdomen.yRot = Mth.cos(limbSwing * 0.33333334F + ((float)Math.PI / 2.0F)) * 0.6F * slitherAmount;
-        abdomen.zRot = Mth.cos(limbSwing * 0.33333334F) * 0.6F * slitherAmount;
+        abdomen.yRot = slitherAnim(segmentPosition, limbSwing * SLITHER_RATE) * 0.5f;
+        abdomen.zRot = 0f;
+        overallAngle += -abdomen.yRot;
+        segmentPosition += SEGMENT_SIZE * 2f;
         lowerAbdomen.xRot = (float) Math.toRadians(42.5);
-        lowerAbdomen.yRot = abdomen.yRot;
-        lowerAbdomen.zRot = abdomen.zRot;
+        lowerAbdomen.yRot = 0f;//abdomen.yRot;
+        lowerAbdomen.zRot = 0f;
         tail.xRot = (float) Math.toRadians(60);
-        tail.yRot = 0.0F;
-        tail.zRot = 0.0F;
+        tail.zRot = 0f;
+        tail.yRot = 0f;
 
-        float offset = 0.0F;
         for (ModelPart joint : tailJoints) {
-            joint.zRot = -0.35F * Mth.cos(limbSwing * 0.33333334F -
-                    (((float)Math.PI / 3.0F) * offset)) * slitherAmount;
-            joint.xRot = 0.0F;
-            joint.yRot = 0.0F;
-            offset += 0.75F;
+            joint.zRot = slitherAngle(segmentPosition, limbSwing * SLITHER_RATE) - overallAngle;
+            joint.yRot = 0f;
+            joint.xRot = 0f;
+            overallAngle += joint.zRot;
+            segmentPosition += SEGMENT_SIZE;
         }
     }
 }
