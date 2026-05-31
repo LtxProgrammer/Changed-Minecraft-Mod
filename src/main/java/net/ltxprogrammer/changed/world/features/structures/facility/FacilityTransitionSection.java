b358573f11117a458d8d7dad97863fe3146051ea
LtxPgm <ltxprogrammer@outlook.com>
2026-05-31 13:37:57 -0500

    Added FacilityPieceEvent registry object that can be configured into facility pieces. Gets ticked and listens to player enter/leave piece triggers. Impl #1766
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityTransitionSection.java
+++ src/main/java/net/ltxprogrammer/changed/world/features/structures/facility/FacilityTransitionSection.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.random.WeightedEntry;
 import net.minecraft.util.random.WeightedRandomList;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -21,6 +22,10 @@ public class FacilityTransitionSection extends FacilitySinglePiece {
 
     public FacilityTransitionSection(ResourceLocation templateName, Optional<ResourceLocation> lootTable) {
         super(ChangedFacilityPieceTypes.TRANSITION.get(), templateName, lootTable);
+    }
+
+    public FacilityTransitionSection(ResourceLocation templateName, Optional<ResourceLocation> lootTable, List<FacilityPieceEvent> events) {
+        super(ChangedFacilityPieceTypes.TRANSITION.get(), templateName, lootTable, events);
     }
 
     @Override
